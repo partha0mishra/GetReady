9b59c615f1a174141e9daab8adfe2200965dfd3e
Partha Mishra <partha.mishra@gmail.com>
2020-07-30 16:44:46 -0500

    WordBreak2 - added another approach

--- algods/src/main/java/com/algods/leetcode/WordBreak2.java
+++ algods/src/main/java/com/algods/leetcode/WordBreak2.java
@@ -75,6 +75,40 @@ public class WordBreak2 {
 		System.out.println(instance.wordBreak("pineapplepenapple", Arrays.asList(wordDict02)));
 		String[] wordDict03= {"cats", "dog", "sand", "and", "cat"};
 		System.out.println(instance.wordBreak("catsandog", Arrays.asList(wordDict03)));
+		
+		System.out.println(instance.wordBreak2("catsanddog", Arrays.asList(wordDict01)));
+		System.out.println(instance.wordBreak2("pineapplepenapple", Arrays.asList(wordDict02)));
+		System.out.println(instance.wordBreak2("catsandog", Arrays.asList(wordDict03)));
 	}
+	/*
+	 * Approach 2: regular DFS with StringBuilder
+	 */
+	public List<String> wordBreak2(String s, List<String> wordDict) {
+//		if (s.length() > 100) return new ArrayList();
 
+		List<String> result = new ArrayList<String> ();
+		wordBreakUtil(s, wordDict, result, new StringBuilder());
+		return result;
+	}
+	public void wordBreakUtil(String s, List<String> wordDict, List<String> result, StringBuilder subList) {
+        // add " " between 2 words in subList
+		if (subList.length() != 0) {
+			subList.append(" ");
+		}
+		// iterate over all the words in wordDict
+        for (String word: wordDict) {
+			
+			if (s.startsWith(word)) {
+				StringBuilder sb = new StringBuilder(subList);
+			    // append current match in sb
+				sb.append(word); 
+			    // if this is last word to be matched
+				if (s.equals(word)) {
+					result.add(new String(sb));
+				} else {
+					wordBreakUtil(s.substring(word.length()), wordDict, result, sb);
+				}
+            }
+		}
+	}
 }
