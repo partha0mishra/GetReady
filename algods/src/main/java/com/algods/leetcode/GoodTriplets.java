a697558227baa05525909d2428465f63749c8b87
Partha Mishra <partha.mishra@gmail.com>
2020-08-02 12:04:35 -0500

    Good Triplets - Brute solution

--- algods/src/main/java/com/algods/leetcode/GoodTriplets.java
+++ algods/src/main/java/com/algods/leetcode/GoodTriplets.java
@@ -28,19 +28,21 @@ Explanation: No triplet satisfies all conditions.
  */
 public class GoodTriplets {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        if(arr.length <=3) return 0;
-        for(int i=0; i<arr.length -1; i++) {
-        	for(int j=i+1; j<arr.length; j++) {
-        		
-        	}
-        }
-        
-        return 1;
+    	int count=0;
+    	for(int i=0; i< arr.length; i++)
+    		for(int j=i+1; j< arr.length; j++)
+    			for(int k=j+1; k< arr.length; k++)
+    				if(Math.abs(arr[i] -arr[j]) <=a && Math.abs(arr[j] - arr[k]) <=b && Math.abs(arr[i]-arr[k]) <= c)
+    					count++;
+        return count;
     }
     
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GoodTriplets instance= new GoodTriplets();
+		int[] arr01= {3,0,1,1,9,7};
+		System.out.println(instance.countGoodTriplets(arr01, 7, 2, 3));// 4
+		int[] arr02= {1,1,2,2,3};
+		System.out.println(instance.countGoodTriplets(arr02, 0, 0, 1));// 0
 	}
 
 }
