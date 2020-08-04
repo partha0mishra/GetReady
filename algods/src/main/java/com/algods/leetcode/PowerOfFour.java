0cd58966f37c270906303280f6a95ae5e9c9c47f
Partha Mishra <partha.mishra@gmail.com>
2020-08-04 09:08:37 -0500

    Power of Four - Math.log and Math.floor. Faster

--- algods/src/main/java/com/algods/leetcode/PowerOfFour.java
+++ algods/src/main/java/com/algods/leetcode/PowerOfFour.java
@@ -11,7 +11,7 @@ package com.algods.leetcode;
  * 
  */
 public class PowerOfFour {
-	//* Approach 03: check if the number is divisible by 2 and sqrt is multiple of 4 
+	/* Approach 03: check if the number is divisible by 2 and sqrt is multiple of 4 
 	public boolean isPowerOfFour(int num) {
         if(num==1 || num==4) return true;
         if(num<=0)
@@ -31,12 +31,11 @@ public class PowerOfFour {
         return false;
     }
 	//*/
-/* Approach 01: using Math.log [4ms 38.9 MB]
+//* Approach 01: using Math.log [4ms 38.9 MB]
 	public boolean isPowerOfFour(int num) {
+		if(num ==0) return false;
         Double logDiv=Math.log(num)/Math.log(4);
-        if(logDiv.intValue() == logDiv) return true;
-        
-        return false;
+        return logDiv == Math.floor(logDiv);
     }
     //*/
 	public static void main(String[] args) {
