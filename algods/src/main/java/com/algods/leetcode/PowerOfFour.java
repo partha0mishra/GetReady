2507e1a8d6a74938610a14605b575e6a24504540
Partha Mishra <partha.mishra@gmail.com>
2020-08-04 08:39:42 -0500

    Power of Four - Loop way - Exceeds time limit

--- algods/src/main/java/com/algods/leetcode/PowerOfFour.java
+++ algods/src/main/java/com/algods/leetcode/PowerOfFour.java
@@ -11,18 +11,32 @@ package com.algods.leetcode;
  * 
  */
 public class PowerOfFour {
-    public boolean isPowerOfFour(int num) {
+//* Approach 02: using loop - Time limit exceeds for 1162261466
+	public boolean isPowerOfFour(int num) {
+        if(num ==1) return true;
+		int base=4;
+        while(base < num)
+        	base=base*4;
+        
+        if (base == num) return true;
+        return false;
+    }
+	//*/
+/* Approach 01: using Math.log [4ms 38.9 MB]
+	public boolean isPowerOfFour(int num) {
         Double logDiv=Math.log(num)/Math.log(4);
         if(logDiv.intValue() == logDiv) return true;
         
         return false;
     }
+    */
 	public static void main(String[] args) {
 		PowerOfFour instance = new PowerOfFour();
 		System.out.println(instance.isPowerOfFour(16));// true
 		System.out.println(instance.isPowerOfFour(5));// false
 		System.out.println(instance.isPowerOfFour(0));// false
 		System.out.println(instance.isPowerOfFour(1));// true
+		System.out.println(instance.isPowerOfFour(1162261466));// true
 	}
 
 }
