88d35ce8495d93d15bc21eb1fa91843becfb02e3
Partha Mishra <partha.mishra@gmail.com>
2020-08-04 09:04:22 -0500

    power of four - a quicker method, needs more understanding

--- algods/src/main/java/com/algods/leetcode/PowerOfFour.java
+++ algods/src/main/java/com/algods/leetcode/PowerOfFour.java
@@ -11,10 +11,19 @@ package com.algods.leetcode;
  * 
  */
 public class PowerOfFour {
-//* Approach 02: using loop - Time limit exceeds for 1162261466
+	//* Approach 03: check if the number is divisible by 2 and sqrt is multiple of 4 
 	public boolean isPowerOfFour(int num) {
-        if(num ==1) return true;
-		int base=4;
+        if(num==1 || num==4) return true;
+        if(num<=0)
+            return false;
+        if((num&(num-1))==0 && ( Math.sqrt(num) % (1<<2))==0)        
+            return true;
+        return false;
+    }
+	    //*/
+	/* Approach 02: using loop - Time limit exceeds for 1162261466. Time complexity O(logY)
+	public boolean isPowerOfFour(int num) {
+		int base=1;
         while(base < num)
         	base=base*4;
         
@@ -29,14 +38,14 @@ public class PowerOfFour {
         
         return false;
     }
-    */
+    //*/
 	public static void main(String[] args) {
 		PowerOfFour instance = new PowerOfFour();
 		System.out.println(instance.isPowerOfFour(16));// true
 		System.out.println(instance.isPowerOfFour(5));// false
 		System.out.println(instance.isPowerOfFour(0));// false
 		System.out.println(instance.isPowerOfFour(1));// true
-		System.out.println(instance.isPowerOfFour(1162261466));// true
+		System.out.println(instance.isPowerOfFour(1162261466));// false
 	}
 
 }
