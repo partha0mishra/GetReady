7cb1403ca917e1c3b6a468fdaf73a0b9a4772171
Mishra <partha.mishra@gmail.com>
2020-08-15 21:38:36 -0500

    Three Consecutive odds - EASY

---
+++ algods/src/main/java/com/algods/leetcode/ThreeConsecutiveOdds.java
@@ -0,0 +1,38 @@
+package com.algods.leetcode;
+/*
+ * Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+ */
+public class ThreeConsecutiveOdds {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        if(arr.length <3) return false;
+        int odds=0;
+        
+        for(int i: arr) {
+        	if(i%2 !=0) {
+        		odds++;
+        		if(odds ==3) return true;
+        	}else {
+        		odds=0;
+        	}
+        }
+        return false;
+    }
+}
