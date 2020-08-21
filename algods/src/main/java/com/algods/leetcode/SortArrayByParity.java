544b2933a96aa5eb5982a8b8fd7a3f40e8e3d8f7
Partha Mishra <partha.mishra@gmail.com>
2020-08-21 09:35:16 -0500

    Sort Array - Evens first and then odds - O(n) and no extra space.

--- algods/src/main/java/com/algods/leetcode/SortArrayByParity.java
+++ algods/src/main/java/com/algods/leetcode/SortArrayByParity.java
@@ -17,21 +17,40 @@ Note:
  */
 import java.util.*;
 public class SortArrayByParity {
-	/* Approach 01: Naive O(n)/ O(n)*/
+	/* Approach 02: Exchange odd with Even */
     public int[] sortArrayByParity(int[] A) {
     	if(A.length < 2) return A;// empty array or with only one element
-    	int[] result=new int[A.length];
-    	ArrayList<Integer> odds= new ArrayList<Integer>();
-    	int index=0;
-    	for(int i: A) {
-    		if(i%2 ==0) result[index++]=i;
-    		else odds.add(i);
+    	int left=-1, right=A.length, hi=A.length-1, lo=0;
+    	while(left < right) {
+    		while(A[++left] %2 ==0)
+    			if(left == hi) break;
+    		while(A[--right] %2 ==1)
+    			if(right == lo) break;
+    		if(left > right) break;
+    		else exchange(A,left,right);
     	}
-    	for(Integer i: odds) {
-    		result[index++]=i;
-    	}
-        return result;
+        return A;
     }
+    private void exchange(int[] A,int left, int right) {
+    	int temp=A[left];
+    	A[left]=A[right];
+    	A[right]=temp;
+    }
+	/* Approach 01: Naive O(n)/ O(n)*/
+//    public int[] sortArrayByParity(int[] A) {
+//    	if(A.length < 2) return A;// empty array or with only one element
+//    	int[] result=new int[A.length];
+//    	ArrayList<Integer> odds= new ArrayList<Integer>();
+//    	int index=0;
+//    	for(int i: A) {
+//    		if(i%2 ==0) result[index++]=i;
+//    		else odds.add(i);
+//    	}
+//    	for(Integer i: odds) {
+//    		result[index++]=i;
+//    	}
+//        return result;
+//    }
 	public static void main(String[] args) {
 		SortArrayByParity instance= new SortArrayByParity();
 		System.out.println(Arrays.toString(instance.sortArrayByParity(new int[] {})));
@@ -39,6 +58,7 @@ public class SortArrayByParity {
 		System.out.println(Arrays.toString(instance.sortArrayByParity(new int[] {1,1})));
 		System.out.println(Arrays.toString(instance.sortArrayByParity(new int[] {0})));
 		System.out.println(Arrays.toString(instance.sortArrayByParity(new int[] {3,1,2,4})));
+		System.out.println(Arrays.toString(instance.sortArrayByParity(new int[] {0,2})));
 	}
 
 }
