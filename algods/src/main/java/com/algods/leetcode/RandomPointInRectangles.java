e893b074e591dbba46ef9f5ae11bd47631e12f1d
Partha Mishra <partha.mishra@gmail.com>
2020-08-22 09:24:47 -0500

    Random Point in Rectangles - copied. cleaner. Still need a more performant solution

--- algods/src/main/java/com/algods/leetcode/RandomPointInRectangles.java
+++ algods/src/main/java/com/algods/leetcode/RandomPointInRectangles.java
@@ -33,40 +33,70 @@ Solution's constructor has one argument, the array of rectangles rects.
 pick has no arguments. Arguments are always wrapped with a list, even if there aren't any.
  */
 import java.util.*;
+/* Approach 02: Copied - Cleaner 
+ * https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/discuss/155452/Java-TreeMap-solution-only-one-random-per-pick*/
 class Solution {
-	TreeMap<Integer, Integer> map;
-    int[][] rects;
-    int sum;
-    Random rnd= new Random();
-    
+    private int[][] rects;
+    private Random r;
+    private TreeMap<Integer, Integer> map;
+    private int area;
+
     public Solution(int[][] rects) {
         this.rects = rects;
+        r = new Random();
         map = new TreeMap<>();
-        sum = 0;
-        
-        for(int i = 0; i < rects.length; i++) {
-            int[] rect = rects[i];
-						
-            // the right part means the number of points can be picked in this rectangle
-            sum += (rect[2] - rect[0] + 1) * (rect[3] - rect[1] + 1);
-			
-            map.put(sum, i);
+        area = 0;
+        for (int i = 0; i < rects.length; i++) {
+            area += (rects[i][2] - rects[i][0] + 1) * (rects[i][3] - rects[i][1] + 1);
+            map.put(area, i);
         }
     }
     
     public int[] pick() {
-        // nextInt(sum) returns a num in [0, sum -1]. After added by 1, it becomes [1, sum]
-        int c = map.ceilingKey( rnd.nextInt(sum) + 1);
-        
-        return pickInRect(rects[map.get(c)]);
-    }
-    
-    private int[] pickInRect(int[] rect) {
-        int left = rect[0], right = rect[2], bot = rect[1], top = rect[3];
-        
-        return new int[]{left + rnd.nextInt(right - left + 1), bot + rnd.nextInt(top - bot + 1) };
+        int randInt = r.nextInt(area);
+        int key = map.higherKey(randInt);
+        int[] rect = rects[map.get(key)];
+        int x = rect[0] + (key - randInt - 1) % (rect[2] - rect[0] + 1);
+        int y = rect[1] + (key - randInt - 1) / (rect[2] - rect[0] + 1);
+        return new int[]{x, y};
     }
 }
+/* Approach 01: Copied
+ * https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/discuss/154130/Java-Solution.-Randomly-pick-a-rectangle-then-pick-a-point-inside.*/
+//class Solution {
+//	TreeMap<Integer, Integer> map;
+//    int[][] rects;
+//    int sum;
+//    Random rnd= new Random();
+//    
+//    public Solution(int[][] rects) {
+//        this.rects = rects;
+//        map = new TreeMap<>();
+//        sum = 0;
+//        
+//        for(int i = 0; i < rects.length; i++) {
+//            int[] rect = rects[i];
+//						
+//            // the right part means the number of points can be picked in this rectangle
+//            sum += (rect[2] - rect[0] + 1) * (rect[3] - rect[1] + 1);
+//			
+//            map.put(sum, i);
+//        }
+//    }
+//    
+//    public int[] pick() {
+//        // nextInt(sum) returns a num in [0, sum -1]. After added by 1, it becomes [1, sum]
+//        int c = map.ceilingKey( rnd.nextInt(sum) + 1);
+//        
+//        return pickInRect(rects[map.get(c)]);
+//    }
+//    
+//    private int[] pickInRect(int[] rect) {
+//        int left = rect[0], right = rect[2], bot = rect[1], top = rect[3];
+//        
+//        return new int[]{left + rnd.nextInt(right - left + 1), bot + rnd.nextInt(top - bot + 1) };
+//    }
+//}
 public class RandomPointInRectangles {
 
 	public static void main(String[] args) {
