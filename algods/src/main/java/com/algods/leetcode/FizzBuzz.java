f154dc2f71f1b5be35960038c336e474e609948c
Partha Mishra <partha.mishra@gmail.com>
2020-08-26 10:50:55 -0500

    FizzBuzz - without IF-Else

--- algods/src/main/java/com/algods/leetcode/FizzBuzz.java
+++ algods/src/main/java/com/algods/leetcode/FizzBuzz.java
@@ -29,16 +29,27 @@ Return:
  */
 import java.util.*;
 public class FizzBuzz {
-    public List<String> fizzBuzz(int n) {
-    	List<String> result= new ArrayList<String>();
-        for(int i=1; i<=n; i++) {
-        	if(i%15 ==0) result.add("FizzBuzz"); 
-        	else if(i%3  ==0) result.add("Fizz");
-        	else if (i%5 ==0) result.add("Buzz");
-        	else result.add(String.valueOf(i));
-        }
-        return result;
-    }
+	/* Approach 02: without if-else */
+	public List<String> fizzBuzz(int n){
+		String[] result=new String[n];
+		for(int i=1; i<=n; i++)    result[i-1]=String.valueOf(i);
+		for(int i=3; i<=n; i+=3)   result[i-1]="Fizz";
+		for(int i=5; i<=n; i+=5)   result[i-1]="Buzz";
+		for(int i=15; i<=n; i+=15) result[i-1]="FizzBuzz";
+		
+		return Arrays.asList(result);
+	}
+	/* Approach 01: Super-easy if-else*/
+//    public List<String> fizzBuzz(int n) {
+//    	List<String> result= new ArrayList<String>();
+//        for(int i=1; i<=n; i++) {
+//        	if(i%15 ==0) result.add("FizzBuzz"); 
+//        	else if(i%3  ==0) result.add("Fizz");
+//        	else if (i%5 ==0) result.add("Buzz");
+//        	else result.add(String.valueOf(i));
+//        }
+//        return result;
+//    }
 	public static void main(String[] args) {
 		FizzBuzz instance= new FizzBuzz();
 		System.out.println(instance.fizzBuzz(0));
