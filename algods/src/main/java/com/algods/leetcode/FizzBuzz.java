ade8a428cfbbc6c3fd47df614c49895baf5d091d
Partha Mishra <partha.mishra@gmail.com>
2020-08-26 11:06:12 -0500

    FizzBuzz - Extensible - with Composite Design Pattern

--- algods/src/main/java/com/algods/leetcode/FizzBuzz.java
+++ algods/src/main/java/com/algods/leetcode/FizzBuzz.java
@@ -29,16 +29,46 @@ Return:
  */
 import java.util.*;
 public class FizzBuzz {
-	/* Approach 02: without if-else */
-	public List<String> fizzBuzz(int n){
-		String[] result=new String[n];
-		for(int i=1; i<=n; i++)    result[i-1]=String.valueOf(i);
-		for(int i=3; i<=n; i+=3)   result[i-1]="Fizz";
-		for(int i=5; i<=n; i+=5)   result[i-1]="Buzz";
-		for(int i=15; i<=n; i+=15) result[i-1]="FizzBuzz";
-		
-		return Arrays.asList(result);
+	/* Approach 03: Composite Design Pattern - more extensible */
+	interface Rule{
+	     boolean apply(int i);
 	}
+	private Map<Rule, String> ruleContainers = new HashMap<Rule,String>();
+	private ArrayList<Rule> rules  = new ArrayList<Rule>();
+	public FizzBuzz() {
+		addRule(i -> i % 15 == 0, "FizzBuzz");
+		addRule(i -> i % 3 == 0, "Fizz");
+		addRule(i -> i % 5 == 0, "Buzz");
+	}
+	public void addRule(Rule rule, String res) {
+		rules.add(rule);
+		ruleContainers.put(rule, res);
+	}
+	public String getValue(int i) {
+        for (Rule rule : rules) {
+            if (rule.apply(i)) {
+                return ruleContainers.get(rule);
+            }
+        }
+        return String.valueOf(i);
+    }
+	public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<String>();
+        for(int i = 1; i <= n; i++){
+            res.add(getValue(i));
+        }
+        return res;
+    }
+	/* Approach 02: without if-else */
+//	public List<String> fizzBuzz(int n){
+//		String[] result=new String[n];
+//		for(int i=1; i<=n; i++)    result[i-1]=String.valueOf(i);
+//		for(int i=3; i<=n; i+=3)   result[i-1]="Fizz";
+//		for(int i=5; i<=n; i+=5)   result[i-1]="Buzz";
+//		for(int i=15; i<=n; i+=15) result[i-1]="FizzBuzz";
+//		
+//		return Arrays.asList(result);
+//	}
 	/* Approach 01: Super-easy if-else*/
 //    public List<String> fizzBuzz(int n) {
 //    	List<String> result= new ArrayList<String>();
