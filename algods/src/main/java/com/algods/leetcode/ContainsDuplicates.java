676e268d2d41dd2059ef29b4687c2303617d680d
Partha Mishra <partha.mishra@gmail.com>
2020-09-02 16:12:20 -0500

    Contains Duplicates - good one

--- algods/src/main/java/com/algods/leetcode/ContainsDuplicates.java
+++ algods/src/main/java/com/algods/leetcode/ContainsDuplicates.java
@@ -19,6 +19,16 @@ package com.algods.leetcode;
  */
 import java.util.*;
 public class ContainsDuplicates {
+	/**
+	 * Approach 01: O(n logK) COPIED
+	 * https://leetcode.com/problems/contains-duplicate-iii/discuss/61655/Java-O(N-lg-K)-solution
+	 * 
+	 * This problem requires to maintain a window of size k of the previous values that can be queried for value ranges. 
+	 * The best data structure to do that is Binary Search Tree. As a result maintaining the tree of size k will result in time 
+	 * complexity O(N lg K). In order to check if there exists any value of range abs(nums[i] - nums[j]) to simple queries 
+	 * can be executed both of time complexity O(lg K). Here is the whole solution using TreeMap.
+	 * 
+	 */
 	public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
 		if (nums.length < 2 || k == 0) {
 			return false;
