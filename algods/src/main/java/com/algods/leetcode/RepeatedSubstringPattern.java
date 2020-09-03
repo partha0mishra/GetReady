7d5423134b96c581f3e5a160d81f6f459f75de43
Partha Mishra <partha.mishra@gmail.com>
2020-09-03 12:13:34 -0500

    Repeated Substring - Worth practice

--- algods/src/main/java/com/algods/leetcode/RepeatedSubstringPattern.java
+++ algods/src/main/java/com/algods/leetcode/RepeatedSubstringPattern.java
@@ -17,28 +17,38 @@ package com.algods.leetcode;
  * Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
  */
 public class RepeatedSubstringPattern {
+	/* Approach 02: COPIED
+	 * https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3447/discuss/94344/Simple-Java-solution-2-lines*/
 	public boolean repeatedSubstringPattern(String s) {
-		if(s.length() < 2) return false;
-//		System.out.println("Length: "+s.length());
-		boolean skipThisLength=false;
-		for(int subLen=1; subLen<= s.length()/2; subLen ++) {
-//			System.out.println("substring length:"+subLen);
-			if(s.length() % subLen !=0) continue;// need it to be a factor
-//			System.out.println("considered");
-			skipThisLength=false;
-			for(int start=subLen; start <= s.length() -subLen; start+=subLen) {
-//				System.out.println("start: "+start);
-				for(int i=0; i< subLen; i++) {
-//					System.out.println("left: "+i+" right: "+(start+i));
-//					System.out.println("left: "+s.charAt(i)+" right: "+s.charAt(start+i));
-					if(s.charAt(i) != s.charAt(start+i)) {skipThisLength=true; break;}
-				}
-				if(skipThisLength) break;
-			}
-			if(! skipThisLength) break;
-		}
-		return !skipThisLength;
+		String s2=s+s;
+//		System.out.println(s);
+//		System.out.println(s2);
+//		System.out.println(s2.substring(1, s2.length()-1));
+		return s2.substring(1, s2.length()-1).contains(s);
     }
+	/* Approach 01: Brute and Naive but it seems this is regular at a high performance zone of submissions */
+//	public boolean repeatedSubstringPattern(String s) {
+//		if(s.length() < 2) return false;
+////		System.out.println("Length: "+s.length());
+//		boolean skipThisLength=false;
+//		for(int subLen=1; subLen<= s.length()/2; subLen ++) {
+////			System.out.println("substring length:"+subLen);
+//			if(s.length() % subLen !=0) continue;// need it to be a factor
+////			System.out.println("considered");
+//			skipThisLength=false;
+//			for(int start=subLen; start <= s.length() -subLen; start+=subLen) {
+////				System.out.println("start: "+start);
+//				for(int i=0; i< subLen; i++) {
+////					System.out.println("left: "+i+" right: "+(start+i));
+////					System.out.println("left: "+s.charAt(i)+" right: "+s.charAt(start+i));
+//					if(s.charAt(i) != s.charAt(start+i)) {skipThisLength=true; break;}
+//				}
+//				if(skipThisLength) break;
+//			}
+//			if(! skipThisLength) break;
+//		}
+//		return !skipThisLength;
+//    }
 	
 	public static void main(String[] args) {
 		RepeatedSubstringPattern instance = new RepeatedSubstringPattern();
