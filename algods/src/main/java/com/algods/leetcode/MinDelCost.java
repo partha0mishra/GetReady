bb7e4111b3cb9015cddea0ed35ca290fa7b0ccb9
Partha Mishra <partha.mishra@gmail.com>
2020-09-05 23:05:49 -0500

    Added details of the questions

--- algods/src/main/java/com/algods/leetcode/MinDelCost.java
+++ algods/src/main/java/com/algods/leetcode/MinDelCost.java
@@ -1,5 +1,39 @@
 package com.algods.leetcode;
+/*
+ * Minimum Deletion Cost to Avoid Repeating Letters
+ * Given a string s and an array of integers cost where cost[i] is the cost of deleting the character i in s.
 
+Return the minimum cost of deletions such that there are no two identical letters next to each other.
+
+Notice that you will delete the chosen characters at the same time, in other words, after deleting a character, the costs of deleting other characters will not change.
+
+ 
+
+Example 1:
+
+Input: s = "abaac", cost = [1,2,3,4,5]
+Output: 3
+Explanation: Delete the letter "a" with cost 3 to get "abac" (String without two identical letters next to each other).
+Example 2:
+
+Input: s = "abc", cost = [1,2,3]
+Output: 0
+Explanation: You don't need to delete any character because there are no identical letters next to each other.
+Example 3:
+
+Input: s = "aabaa", cost = [1,2,3,4,1]
+Output: 2
+Explanation: Delete the first and the last character, getting the string ("aba").
+ 
+
+Constraints:
+
+s.length == cost.length
+1 <= s.length, cost.length <= 10^5
+1 <= cost[i] <= 10^4
+s contains only lowercase English letters.
+
+ */
 public class MinDelCost {
     public int minCost(String s, int[] cost) {
     	int result=0;
@@ -29,12 +63,10 @@ public class MinDelCost {
     }
 	public static void main(String[] args) {
 		MinDelCost instance= new MinDelCost();
-//		System.out.println(instance.minCost("abc", new int[]{1,2,3}));//0
-//		System.out.println(instance.minCost("a", new int[]{1}));// 0
-//		System.out.println(instance.minCost("abaac", new int[]{1,2,3,4,5}));
-		System.out.println(instance.minCost("aabaa", new int[]{1,2,3,4,1}));
-//		System.out.println(instance.minCost("abc", new int[]{1,2,3}));
-//		System.out.println(instance.minCost("abc", new int[]{1,2,3}));
+		System.out.println(instance.minCost("abc", new int[]{1,2,3}));//0
+		System.out.println(instance.minCost("a", new int[]{1}));// 0
+		System.out.println(instance.minCost("abaac", new int[]{1,2,3,4,5}));//3
+		System.out.println(instance.minCost("aabaa", new int[]{1,2,3,4,1}));//2
 	}
 
 }
