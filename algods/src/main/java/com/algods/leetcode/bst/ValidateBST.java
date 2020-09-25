e6b22cca9f08dbbd7f0e09942e01756af531b1ed
Partha Mishra <partha.mishra@gmail.com>
2020-09-24 21:35:28 -0500

    Validate BST - BOLD :D

---
+++ algods/src/main/java/com/algods/leetcode/bst/ValidateBST.java
@@ -0,0 +1,39 @@
+package com.algods.leetcode.bst;
+
+import com.algods.leetcode.bst.RangeSumBST.TreeNode;
+
+public class ValidateBST {
+	/**
+	 * Definition for a binary tree node.*/
+	 class TreeNode {
+	     int val;
+	     TreeNode left;
+	     TreeNode right;
+	     TreeNode() {}
+	     TreeNode(int val) { this.val = val; }
+	     TreeNode(int val, TreeNode left, TreeNode right) {
+	         this.val = val;
+	         this.left = left;
+	         this.right = right;
+	     }
+	 }
+	int tmp=Integer.MIN_VALUE;
+	public boolean isValidBST(TreeNode root) {
+		if(root.left == null && root.right == null) {
+			return validate(root.val);
+		}else if(root.left == null) {
+			return validate(root.val) && isValidBST(root.right);
+		}else
+			return isValidBST(root.left) && validate(root.val) && isValidBST(root.right);
+	}
+	private boolean validate(int val) {
+		boolean b=val>tmp;
+		tmp=val;
+		return b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
