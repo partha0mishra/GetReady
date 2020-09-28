0f5742cf7b9d3fd42cf993b1e4cc1cfc170d2983
Partha Mishra <partha.mishra@gmail.com>
2020-09-28 09:24:22 -0500

    bettered but still getting TLE and MLE

--- algods/src/main/java/com/algods/leetcode/explore/september/SubarrayProductLessThanK.java
+++ algods/src/main/java/com/algods/leetcode/explore/september/SubarrayProductLessThanK.java
@@ -19,23 +19,51 @@ Note:
 * Hint:
 * For each j, let opt(j) be the smallest i so that nums[i] * nums[i+1] * ... * nums[j] is less than k. opt is an increasing function.
  */
+import java.util.*;
 import static org.junit.Assert.assertEquals;
 public class SubarrayProductLessThanK {
-	/* Approach 01: Brute force - TLE*/
 	public int numSubarrayProductLessThanK(int[] nums, int k) {
-		if(nums.length ==0) return 0;
-        int result=0;
-        for(int i=0; i< nums.length; i++) {
-        	int prod=1;
-        	for(int j=i; j< nums.length; j++) {
-        		prod*=nums[j];
-//        		System.out.println(prod);
-        		if(prod<k) result++;
-        		else break;
-        	}
-        }
-        return result;
-    }
+		if(nums.length == 0) return 0;
+		int result=0;
+		ArrayList<Integer> products=new ArrayList<>();
+		for(int i=nums.length-1; i>= 0; i--) {
+//			System.out.println(nums[i]);
+			if(nums[i] > k) {// start afresh
+				products=new ArrayList<Integer>();
+				continue;
+			}else {
+				ArrayList<Integer> tempProducts= new ArrayList<Integer>();
+				tempProducts.add(nums[i]);
+				for(int j=0; j< products.size(); j++) {
+					if(nums[i] ==0) tempProducts.add(0);
+					if(nums[i] ==1) tempProducts.addAll(products);
+					int p=products.get(j)*nums[i];
+					System.out.println(p);
+					if(p < k) {
+						tempProducts.add(p);
+					}else break;// dead combination hereafter
+				}
+				result+=tempProducts.size();
+				products=tempProducts;
+			}
+		}
+		return result;
+	}
+	/* Approach 01: Brute force - TLE*/
+//	public int numSubarrayProductLessThanK(int[] nums, int k) {
+//		if(nums.length ==0) return 0;
+//        int result=0;
+//        for(int i=0; i< nums.length; i++) {
+//        	int prod=1;
+//        	for(int j=i; j< nums.length; j++) {
+//        		prod*=nums[j];
+////        		System.out.println(prod);
+//        		if(prod<k) result++;
+//        		else break;
+//        	}
+//        }
+//        return result;
+//    }
 	public static void main(String[] args) {
 		assertEquals(8,new SubarrayProductLessThanK().numSubarrayProductLessThanK(new int[] {10, 5, 2, 6}, 100));
 	}
