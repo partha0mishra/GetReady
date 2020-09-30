c5c1c2d31454b5b128b3998ed0c2233a1cfaf67b
Partha Mishra <partha.mishra@gmail.com>
2020-09-29 21:35:58 -0500

    Negative Integers Sorted Matrix - Lovely solution

--- algods/src/main/java/com/algods/leetcode/arrays/NegativeIntegersSortedMatrix.java
+++ algods/src/main/java/com/algods/leetcode/arrays/NegativeIntegersSortedMatrix.java
@@ -34,11 +34,24 @@ n == grid[i].length
 -100 <= grid[i][j] <= 100
  * */
 public class NegativeIntegersSortedMatrix {
+	/* Approach 02: 
+	 * Start with Top right: if it's <0 the rest of the column at bottom is <0. Go left and repeat.
+	 * Else, go down and Repeat.*/
 	public int countNegatives(int[][] grid) {
-        int count=0, cols=grid[0].length;
-        for(int r=0; r< grid.length; r++)
-            for(int c=0; c< cols; c++)
-                if(grid[r][c] < 0) {count+=cols-c; break;}
-        return count;
-    }
+		int rows=grid.length, cols=grid[0].length, r=0, c=cols-1, count=0;
+		while(r < rows && c >=0) {
+			int start=grid[r][c];
+			if(start < 0) {count+=rows-r;c--;}
+			else r++;
+		}
+		return count;
+	}
+	/* Approach 01: Brute with some optimization*/
+//	public int countNegatives(int[][] grid) {
+//        int count=0, cols=grid[0].length;
+//        for(int r=0; r< grid.length; r++)
+//            for(int c=0; c< cols; c++)
+//                if(grid[r][c] < 0) {count+=cols-c; break;}
+//        return count;
+//    }
 }
