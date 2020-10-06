2d51b5469d57f46bf019eeacc96310a15219d086
Partha Mishra <partha.mishra@gmail.com>
2020-10-06 08:52:45 -0500

    BST Insertion - Easy

--- algods/src/main/java/com/algods/leetcode/bst/BSTInsertion.java
+++ algods/src/main/java/com/algods/leetcode/bst/BSTInsertion.java
@@ -49,11 +49,10 @@ public class BSTInsertion {
 	     }
 	 }
 	 public TreeNode insertIntoBST(TreeNode root, int val) {
-	        if(root==null) {
-	        	root=new TreeNode(val);
-	        	return root;
-	        }
-	        if(val < root.val) return insertIntoBST(root.left,val);
-	        else return insertIntoBST(root.right,val);
+	        if(root==null) return new TreeNode(val);
+	        if(val < root.val) root.left= insertIntoBST(root.left,val);
+	        else root.right= insertIntoBST(root.right,val);
+     
+     return root;
 	 }
 }
