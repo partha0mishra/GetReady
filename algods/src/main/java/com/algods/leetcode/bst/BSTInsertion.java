71aa7266683a5dd2c807050a94aa0041e919aa92
Partha Mishra <partha.mishra@gmail.com>
2020-10-06 11:21:06 -0500

    Merge Two BSTs - easy

--- algods/src/main/java/com/algods/leetcode/bst/BSTInsertion.java
+++ algods/src/main/java/com/algods/leetcode/bst/BSTInsertion.java
@@ -34,20 +34,6 @@ All the values Node.val are unique.
 It's guaranteed that val does not exist in the original BST.
  * */
 public class BSTInsertion {
-	/**
-	 * Definition for a binary tree node.*/
-	 class TreeNode {
-	     int val;
-	     TreeNode left;
-	     TreeNode right;
-	     TreeNode() {}
-	     TreeNode(int val) { this.val = val; }
-	     TreeNode(int val, TreeNode left, TreeNode right) {
-	         this.val = val;
-	         this.left = left;
-	         this.right = right;
-	     }
-	 }
 	 public TreeNode insertIntoBST(TreeNode root, int val) {
 	        if(root==null) return new TreeNode(val);
 	        if(val < root.val) root.left= insertIntoBST(root.left,val);
