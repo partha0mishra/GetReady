1957e835d55039fc4003ed99400152e95e80569a
Partha Mishra <partha.mishra@gmail.com>
2020-10-06 16:36:46 -0500

    Curved out Node class for an N-ary node

--- algods/src/main/java/com/algods/leetcode/bst/NaryTreePostOrderTraversal.java
+++ algods/src/main/java/com/algods/leetcode/bst/NaryTreePostOrderTraversal.java
@@ -20,21 +20,6 @@ import com.algods.leetcode.bst.NaryTreePreOrderTraversal.Node;
  * */
 import java.util.*;
 public class NaryTreePostOrderTraversal {
-	class Node {
-	    public int val;
-	    public List<Node> children;
-
-	    public Node() {}
-
-	    public Node(int _val) {
-	        val = _val;
-	    }
-
-	    public Node(int _val, List<Node> _children) {
-	        val = _val;
-	        children = _children;
-	    }
-	}
 	/* Approach 02: Iterative */
 	public List<Integer> postorder(Node root) {
         List<Integer> list = new ArrayList<>();
--- algods/src/main/java/com/algods/leetcode/bst/NaryTreePreOrderTraversal.java
+++ algods/src/main/java/com/algods/leetcode/bst/NaryTreePreOrderTraversal.java
@@ -18,24 +18,6 @@ package com.algods.leetcode.bst;
  * */
 import java.util.*;
 public class NaryTreePreOrderTraversal {
-	/*
-	// Definition for a Node.*/
-	class Node {
-	    public int val;
-	    public List<Node> children;
-
-	    public Node() {}
-
-	    public Node(int _val) {
-	        val = _val;
-	    }
-
-	    public Node(int _val, List<Node> _children) {
-	        val = _val;
-	        children = _children;
-	    }
-	}
-	//*/
 	/* Approach 02: Iterative */
 	public List<Integer> preorder(Node root) {
         List<Integer> list = new ArrayList<>();
