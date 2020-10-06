4a11f369ae30665bbd5c135f2b3b41d8b752901c
Partha Mishra <partha.mishra@gmail.com>
2020-10-05 22:52:01 -0500

    Self Dividing Numbers - brute. easy

--- algods/src/main/java/com/algods/leetcode/math/DigitProductMinusSum.java
+++ algods/src/main/java/com/algods/leetcode/math/DigitProductMinusSum.java
@@ -1,5 +1,39 @@
 package com.algods.leetcode.math;
+/* 1281. Subtract the Product and Sum of Digits of an Integer
+ * Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
 
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+
+Constraints:
+
+1 <= n <= 10^5
+ * */
 public class DigitProductMinusSum {
-
+	public int subtractProductAndSum(int n) {
+        long product=1, sum=0;
+        while( n> 0) {
+        	int digit=n%10;
+        	product*=digit;
+        	sum+=digit;
+        	n/=10;
+        }
+        return (int)(product-sum);
+    }
 }
