50d6dc924cf0cb93a0452166158e04cd144cca5b
Partha Mishra <partha.mishra@gmail.com>
2020-10-06 11:20:52 -0500

    Adding utility TreeNode for all the BSTs

---
+++ algods/src/main/java/com/algods/leetcode/bst/TreeNode.java
@@ -0,0 +1,16 @@
+package com.algods.leetcode.bst;
+/**
+ * Definition of a binary tree node.*/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
