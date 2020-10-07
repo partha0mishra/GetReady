631b447024b3af5d43a353a719676dd0a9cfa326
Partha Mishra <partha.mishra@gmail.com>
2020-10-06 21:07:37 -0500

    Leaf Similar BSTs - dfs with Stacks - similar to Queue, just another way

--- algods/src/main/java/com/algods/leetcode/bst/LeafSimilarBSTs.java
+++ algods/src/main/java/com/algods/leetcode/bst/LeafSimilarBSTs.java
@@ -25,21 +25,39 @@ Both of the given trees will have values in the range [0, 200].
  * */
 import java.util.*;
 public class LeafSimilarBSTs {
+	/* Approach 02: dfs and keeping leaves in stack */
 	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        LinkedList<Integer> list1= new LinkedList<Integer>();
-        LinkedList<Integer> list2= new LinkedList<Integer>();
-        traverse(root1,list1);
-        traverse(root2,list2);
-        if(list1.size() != list2.size()) return false;
-        for(int i=0; i< list1.size(); i++){
-            if(list1.get(i) != list2.get(i)) return false;
+        Stack<TreeNode> s1 = new Stack<>(), s2 = new Stack<>();
+        s1.push(root1); s2.push(root2);
+        while (!s1.empty() && !s2.empty())
+            if (dfs(s1) != dfs(s2)) return false;
+        return s1.empty() && s2.empty();
+    }
+
+    public int dfs(Stack<TreeNode> s) {
+        while (true) {
+            TreeNode node = s.pop();
+            if (node.right != null) s.push(node.right);
+            if (node.left != null) s.push(node.left);
+            if (node.left == null && node.right == null) return node.val;
         }
-        return true;
     }
-	private void traverse(TreeNode node, LinkedList<Integer> list){
-        if(node == null) return;
-        if(node.left==null && node.right==null) list.add(node.val);
-        traverse(node.left,list);
-        traverse(node.right,list);
-    }
+	/* Approach 01: In order traversal and keeping leaves in Queues/ LinkedLists */
+//	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+//        LinkedList<Integer> list1= new LinkedList<Integer>();
+//        LinkedList<Integer> list2= new LinkedList<Integer>();
+//        traverse(root1,list1);
+//        traverse(root2,list2);
+//        if(list1.size() != list2.size()) return false;
+//        for(int i=0; i< list1.size(); i++){
+//            if(list1.get(i) != list2.get(i)) return false;
+//        }
+//        return true;
+//    }
+//	private void traverse(TreeNode node, LinkedList<Integer> list){
+//        if(node == null) return;
+//        if(node.left==null && node.right==null) list.add(node.val);
+//        traverse(node.left,list);
+//        traverse(node.right,list);
+//    }
 }
