ce10ca342f92c22414aeff0edf755289dfc799eb
Partha Mishra <partha.mishra@gmail.com>
2020-10-09 22:35:06 -0500

    Kth Largest - using PriorityQueue - MinHeap

--- algods/src/main/java/com/algods/leetcode/heap/KthLargest.java
+++ algods/src/main/java/com/algods/leetcode/heap/KthLargest.java
@@ -36,27 +36,49 @@ Constraints:
 -104 <= val <= 104
 At most 104 calls will be made to add.
  */
+import java.util.*;
 public class KthLargest {
-	Integer[] items;
-	int k;
-	
-	public KthLargest(int k, int[] nums) {
-		this.k=k;
-		items=new Integer[k+1];
-        Arrays.sort(nums);
-        for(int i=0; i<=k; i++) {// initializing items from back
-        	if(i < nums.length) items[k-i]=nums[nums.length -1-i];
-        	else items[k-i]=Integer.MIN_VALUE;
-        }
+	/* Approach 02: Using PriorityQueue */
+	final PriorityQueue<Integer> q;
+    final int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        q = new PriorityQueue<>(k);
+        for (int n : nums)
+            add(n);
     }
-    
+
     public int add(int val) {
-    	if(val > items[0]) {
-    		items[0]=val;
-    	}
-        Arrays.sort(items);
-        return items[1];
+        if (q.size() < k)// not full yet
+            q.offer(val);
+        else if (q.peek() < val) {// full, with a value less than 'val'
+            q.poll();
+            q.offer(val);
+        }
+        return q.peek();
     }
+	/* Approach 01: using Arrays.sort() */
+//	Integer[] items;
+//	int k;
+//	
+//	public KthLargest(int k, int[] nums) {
+//		this.k=k;
+//		items=new Integer[k+1];
+//        Arrays.sort(nums);
+//        for(int i=0; i<=k; i++) {// initializing items from back
+//        	if(i < nums.length) items[k-i]=nums[nums.length -1-i];
+//        	else items[k-i]=Integer.MIN_VALUE;
+//        }
+//    }
+//    
+//    public int add(int val) {
+//    	if(val > items[0]) {
+//    		items[0]=val;
+//    	}
+//        Arrays.sort(items);
+//        return items[1];
+//    }
     
 	public static void main(String[] args) {
 		KthLargest kthLargest = new KthLargest(3, new int[] {4, 5, 8, 2});
