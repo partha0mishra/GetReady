27c778969495ee7bd2323898da86ef232c21d5e2
Partha Mishra <partha.mishra@gmail.com>
2020-10-11 23:00:18 -0500

    Scor eof parentheses - more

--- algods/src/main/java/com/algods/leetcode/stack/ScoreOfParentheses.java
+++ algods/src/main/java/com/algods/leetcode/stack/ScoreOfParentheses.java
@@ -32,26 +32,55 @@ S is a balanced parentheses string, containing only ( and ).
  */
 import java.util.*;
 public class ScoreOfParentheses {
+	/* Approach 02: Stack - only keeping the score
+	 * cur record the score at the current layer level.
+
+If we meet '(',
+we push the current score to stack,
+enter the next inner layer level,
+and reset cur = 0.
+
+If we meet ')',
+the cur score will be doubled and will be at least 1.
+We exit the current layer level,
+and set cur = stack.pop() + cur
+
+Complexity: O(N) time and O(N) space */
 	public int scoreOfParentheses(String S) {
-        Stack<String> ss= new Stack<String>();
-        int num=0;
-        for(char c: S.toCharArray()) {
-        	if(c == '(') ss.push(String.valueOf(c));
-        	else {// ")"
-        		String p=ss.pop();
-        		if(p.equals("(")) {
-        			num=1;
-        		}else {// it's a number
-        			num=2*Integer.parseInt(p);
-        			ss.pop();// pop out the starting brace
-        		}
-        		if(!ss.isEmpty()) {
-    				String q=ss.peek();
-    				if(! q.equals("(")) num=num+Integer.valueOf(ss.pop());
-    			}
-        		ss.push(String.valueOf(num));
-        	}
+        Stack<Integer> stack = new Stack<>();
+        int cur = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(') {
+                stack.push(cur);
+                cur = 0;
+            } else {
+                cur = stack.pop() + Math.max(cur * 2, 1);
+            }
         }
-        return num;
+        return cur;
     }
+	
+	/* Approach 01: Stack - crude version*/
+//	public int scoreOfParentheses(String S) {
+//        Stack<String> ss= new Stack<String>();
+//        int num=0;
+//        for(char c: S.toCharArray()) {
+//        	if(c == '(') ss.push(String.valueOf(c));
+//        	else {// ")"
+//        		String p=ss.pop();
+//        		if(p.equals("(")) {
+//        			num=1;
+//        		}else {// it's a number
+//        			num=2*Integer.parseInt(p);
+//        			ss.pop();// pop out the starting brace
+//        		}
+//        		if(!ss.isEmpty()) {
+//    				String q=ss.peek();
+//    				if(! q.equals("(")) num=num+Integer.valueOf(ss.pop());
+//    			}
+//        		ss.push(String.valueOf(num));
+//        	}
+//        }
+//        return num;
+//    }
 }
