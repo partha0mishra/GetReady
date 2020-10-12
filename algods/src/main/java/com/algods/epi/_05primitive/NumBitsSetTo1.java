182823ba2fe3ca1c10a7dd4988b99625642a30d7
Partha Mishra <partha.mishra@gmail.com>
2020-10-12 15:26:53 -0500

    epi - number of bits set to 1 - easy method

---
+++ algods/src/main/java/com/algods/epi/_05primitive/NumBitsSetTo1.java
@@ -0,0 +1,12 @@
+package com.algods.epi._05primitive;
+
+public class NumBitsSetTo1 {
+	public static short countBits(int i) {
+		return (short)Integer.bitCount(i);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
