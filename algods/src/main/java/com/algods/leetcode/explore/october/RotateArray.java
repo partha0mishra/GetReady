4a2374b74b9f210cc8e3e3853dca2f2477c36d6f
Partha Mishra <partha.mishra@gmail.com>
2020-10-15 15:31:21 -0500

    Rotated Array - Cyclic ordering O(n) O(1)

--- algods/src/main/java/com/algods/leetcode/explore/october/RotateArray.java
+++ algods/src/main/java/com/algods/leetcode/explore/october/RotateArray.java
@@ -31,16 +31,31 @@ Constraints:
 0 <= k <= 105
 */
 public class RotateArray {
-	/* Approach 01: use extra array O(n) O(n)*/
+	/* Approach 02: cyclic dependency O(n) O(1)*/
 	public void rotate(int[] nums, int k) {
-		if(k ==0) return;
-		int n=nums.length;
-        int[] result=new int[n];
-        for(int from=0; from< n; from++) {
-        	result[(from+k)%n]=nums[from];
-        }
-        for(int i=0; i< n; i++){
-            nums[i]=result[i];
-        }
+		int n=nums.length, count=0;
+		k=k%n;
+		for(int start=0; count< n; start++) {
+			int from=start;
+			int val=nums[from];
+			do {
+				int to=(from+k)%n;
+				int temp=nums[to];nums[to]=val;
+				val=temp;from=to;
+				count+=1;
+			}while(from !=start);
+		}
     }
+	/* Approach 01: use extra array O(n) O(n)*/
+//	public void rotate(int[] nums, int k) {
+//		if(k ==0) return;
+//		int n=nums.length;
+//        int[] result=new int[n];
+//        for(int from=0; from< n; from++) {
+//        	result[(from+k)%n]=nums[from];
+//        }
+//        for(int i=0; i< n; i++){
+//            nums[i]=result[i];
+//        }
+//    }
 }
