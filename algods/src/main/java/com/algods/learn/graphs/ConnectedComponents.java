5b6f984ce54b3cf1499a036839cf1d8ab6740d55
Partha Mishra <partha.mishra@gmail.com>
2020-10-28 16:22:21 -0500

    Connected Components - took care of Size[]

--- algods/src/main/java/com/algods/learn/graphs/ConnectedComponents.java
+++ algods/src/main/java/com/algods/learn/graphs/ConnectedComponents.java
@@ -8,18 +8,20 @@ public class ConnectedComponents {
 	public ConnectedComponents(Graph graph) {
 		marked= new boolean[graph.V()];
 		id= new int[graph.V()];
-		size= new int[graph.V()];
+//		size= new int[graph.V()];
 		for(int v=0; v<graph.V(); v++) {// check for all vertices
 			if(!marked[v]) {
 				dfs(graph, v);
 				count++;
 			}
 		}
+		size=new int[count+1];
+		for(int c: id) size[c]+=1;
 	}
 	public void dfs(Graph graph, int node) {
 		marked[node]=true;// marked in the dfs path
 		id[node]=count;// the CC this node belongs to
-		size[count]++;// adding 1 to the size of this CC
+//		size[count]++;// adding 1 to the size of this CC
 		for(int w: graph.adj(node)) {
 			if(!marked[w])
 			dfs(graph, w);
@@ -34,6 +36,7 @@ public class ConnectedComponents {
 	public boolean connected(int node1, int node2) {
 		return id[node1] == id[node2];
 	}
+	public int size(int c) { return size[c];}
 	public static void main(String[] args) {
 		Graph graph= new Graph(12);
 		graph.addEdge(0, 1);
@@ -53,7 +56,7 @@ public class ConnectedComponents {
 		ConnectedComponents cc= new ConnectedComponents(graph);
 		int m=cc.count();
 		System.out.println("Number of Components: "+m);
-		
+		for(int i=0; i< m; i++) System.out.println("Size of CC "+(i+1)+": "+cc.size(i));
 	}
 
 }
