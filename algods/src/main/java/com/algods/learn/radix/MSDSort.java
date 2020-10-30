6314e1296b5d4c59d7d0cd0d66d27817eec8019d
Partha Mishra <partha.mishra@gmail.com>
2020-10-30 09:59:37 -0500

    longest Increasing subsequence - copied

--- algods/src/main/java/com/algods/learn/radix/MSDSort.java
+++ algods/src/main/java/com/algods/learn/radix/MSDSort.java
@@ -25,29 +25,15 @@ public class MSDSort {
 
         // compute frequency counts
         int[] count = new int[R+2];
-        for (int i = lo; i <= hi; i++) {
-            int c = charAt(a[i], d);
-            count[c+2]++;
-        }
-
-        // transform counts to indicies
-        for (int r = 0; r < R+1; r++)
-            count[r+1] += count[r];
-
+        for (int i = lo; i <= hi; i++) {int c = charAt(a[i], d); count[c+2]++;}
+        // transform counts to indices
+        for (int r = 0; r < R+1; r++) count[r+1] += count[r];
         // distribute
-        for (int i = lo; i <= hi; i++) {
-            int c = charAt(a[i], d);
-            aux[count[c+1]++] = a[i];
-        }
-
+        for (int i = lo; i <= hi; i++) {int c = charAt(a[i], d);aux[count[c+1]++] = a[i];}
         // copy back
-        for (int i = lo; i <= hi; i++) 
-            a[i] = aux[i - lo];
-
-
+        for (int i = lo; i <= hi; i++) a[i] = aux[i - lo];
         // recursively sort for each character (excludes sentinel -1)
-        for (int r = 0; r < R; r++)
-            sort(a, lo + count[r], lo + count[r+1] - 1, d+1, aux);
+        for (int r = 0; r < R; r++) sort(a, lo + count[r], lo + count[r+1] - 1, d+1, aux);
 	}
 	// insertion sort a[lo..hi], starting at dth character
     private static void insertion(String[] a, int lo, int hi, int d) {
