a38444a765ec6749fa2676931b57ecfa11153f75
Partha Mishra <partha.mishra@gmail.com>
2020-11-07 12:31:28 -0600

    Add Two Numbers 2 - Stack based

--- algods/src/main/java/com/algods/leetcode/linkedList/AddTwoNumbers2.java
+++ algods/src/main/java/com/algods/leetcode/linkedList/AddTwoNumbers2.java
@@ -12,34 +12,65 @@ Example:
 Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 8 -> 0 -> 7
  */
+import java.util.*;
 public class AddTwoNumbers2 {
+	/* Approach 02: Using Stack
+	 * */
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        StringBuilder sb1=new StringBuilder(),sb2=new StringBuilder();
-        while(l1 != null) {sb1.append(l1.val);l1=l1.next;}
-        while(l2 != null) {sb2.append(l2.val);l2=l2.next;}
+        Stack<Integer> s1 = new Stack<Integer>(),s2 = new Stack<Integer>();
         
-        int maxLen=Math.max(sb1.length(), sb2.length());
-        // System.out.println(maxLen);
-        while(sb1.length() < maxLen) sb1.insert(0, 0);
-        // System.out.println(sb1);
-        while(sb2.length() < maxLen) sb2.insert(0, 0);
-        // System.out.println(sb2);
-        int n=0;
-        ListNode result=null;
-        for(int i=maxLen-1; i>=0; i--) {
-            // System.out.println(i+" "+sb1.charAt(i)+" "+sb2.charAt(i));
-        	n+=(sb1.charAt(i) - '0') +(sb2.charAt(i) -'0');
-        	// System.out.println(n);
-        	ListNode temp=new ListNode(n%10);
-        	n=n/10;
-        	temp.next=result;
-        	result=temp;
+        while(l1 != null) {s1.push(l1.val);l1 = l1.next;}
+        while(l2 != null) {s2.push(l2.val);l2 = l2.next;}
+        
+        int n = 0;
+        ListNode result = null;
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) n += s1.pop();
+            if (!s2.empty()) n += s2.pop();
+            ListNode temp=new ListNode(n%10);
+            temp.next=result;
+            result=temp;
+            n /= 10;
         }
-        if(n > 0){
-            ListNode temp=new ListNode(n);
+        if(n > 0) {
+        	ListNode temp=new ListNode(n);
             temp.next=result;
             result=temp;
         }
+        
         return result;
     }
+	/* Approach 01a: using int1 to keep l1 and int2 to keep l2 and int3=int1+int2 and convert back to LL - too small
+	 * Approach 01b: using long instead - still get TLE as the number of digits can be 10000 
+	 * Approach 01c: converting LLs to StringBuilders and then operating from the END << idea of Stack perhaps??
+	 * */
+//	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        StringBuilder sb1=new StringBuilder(),sb2=new StringBuilder();
+//        while(l1 != null) {sb1.append(l1.val);l1=l1.next;}
+//        while(l2 != null) {sb2.append(l2.val);l2=l2.next;}
+//        
+//        int maxLen=Math.max(sb1.length(), sb2.length());
+//        // System.out.println(maxLen);
+//        while(sb1.length() < maxLen) sb1.insert(0, 0);
+//        // System.out.println(sb1);
+//        while(sb2.length() < maxLen) sb2.insert(0, 0);
+//        // System.out.println(sb2);
+//        int n=0;
+//        ListNode result=null;
+//        for(int i=maxLen-1; i>=0; i--) {
+//            // System.out.println(i+" "+sb1.charAt(i)+" "+sb2.charAt(i));
+//        	n+=(sb1.charAt(i) - '0') +(sb2.charAt(i) -'0');
+//        	// System.out.println(n);
+//        	ListNode temp=new ListNode(n%10);
+//        	n=n/10;
+//        	temp.next=result;
+//        	result=temp;
+//        }
+//        if(n > 0){
+//            ListNode temp=new ListNode(n);
+//            temp.next=result;
+//            result=temp;
+//        }
+//        return result;
+//    }
 }
