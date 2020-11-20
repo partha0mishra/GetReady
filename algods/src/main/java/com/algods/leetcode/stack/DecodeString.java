332a083978d3a0ebce0f06af9887074418a7f36e
Partha Mishra <partha.mishra@gmail.com>
2020-11-19 20:40:10 -0600

    LC Decode String - recursive. more elegant

--- algods/src/main/java/com/algods/leetcode/stack/DecodeString.java
+++ algods/src/main/java/com/algods/leetcode/stack/DecodeString.java
@@ -38,39 +38,65 @@ All the integers in s are in the range [1, 300].
 import java.util.*;
 public class DecodeString {
 	public String decodeString(String s) {
-        String res = "";
-        Stack<Integer> countStack = new Stack<>();
-        Stack<String> resStack = new Stack<>();
-        int idx = 0;
-        while (idx < s.length()) {
-            if (Character.isDigit(s.charAt(idx))) {
-                int count = 0;
-                while (Character.isDigit(s.charAt(idx))) {
-                    count = 10 * count + (s.charAt(idx) - '0');
-                    idx++;
-                }
-                countStack.push(count);
-            }
-            else if (s.charAt(idx) == '[') {
-                resStack.push(res);
-                res = "";
-                idx++;
-            }
-            else if (s.charAt(idx) == ']') {
-                StringBuilder temp = new StringBuilder (resStack.pop());
-                int repeatTimes = countStack.pop();
-                for (int i = 0; i < repeatTimes; i++) {
-                    temp.append(res);
-                }
-                res = temp.toString();
-                idx++;
-            }
-            else {
-                res += s.charAt(idx++);
+        Deque<Character> queue = new ArrayDeque<>();
+        for (char c : s.toCharArray()) queue.offer(c);
+        return helper(queue);
+    }
+    
+    public String helper(Deque<Character> queue) {
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+        while (!queue.isEmpty()) {
+            char c= queue.poll();
+            if (Character.isDigit(c)) {
+                num = num * 10 + c - '0';
+            } else if (c == '[') {
+                String sub = helper(queue);
+                for (int i = 0; i < num; i++) sb.append(sub);   
+                num = 0;
+            } else if (c == ']') {
+                break;
+            } else {
+                sb.append(c);
             }
         }
-        return res;
+        return sb.toString();
     }
+    /* Approach 01: using stack - simulating recursion */
+//	public String decodeString(String s) {
+//        String res = "";
+//        Stack<Integer> countStack = new Stack<>();
+//        Stack<String> resStack = new Stack<>();
+//        int idx = 0;
+//        while (idx < s.length()) {
+//            if (Character.isDigit(s.charAt(idx))) {
+//                int count = 0;
+//                while (Character.isDigit(s.charAt(idx))) {
+//                    count = 10 * count + (s.charAt(idx) - '0');
+//                    idx++;
+//                }
+//                countStack.push(count);
+//            }
+//            else if (s.charAt(idx) == '[') {
+//                resStack.push(res);
+//                res = "";
+//                idx++;
+//            }
+//            else if (s.charAt(idx) == ']') {
+//                StringBuilder temp = new StringBuilder (resStack.pop());
+//                int repeatTimes = countStack.pop();
+//                for (int i = 0; i < repeatTimes; i++) {
+//                    temp.append(res);
+//                }
+//                res = temp.toString();
+//                idx++;
+//            }
+//            else {
+//                res += s.charAt(idx++);
+//            }
+//        }
+//        return res;
+//    }
 	public static void main(String[] args) {
 //		System.out.println(new DecodeString().decodeString("a"));
 //		System.out.println(new DecodeString().decodeString("aa"));
