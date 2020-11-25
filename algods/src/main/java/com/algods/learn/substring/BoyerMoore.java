9f92f53ee649da2ef88aeab85d0fa452fd587b08
Partha Mishra <partha.mishra@gmail.com>
2020-11-25 10:13:24 -0600

    Smallest integer divisible by K - maths problem

--- algods/src/main/java/com/algods/learn/substring/BoyerMoore.java
+++ algods/src/main/java/com/algods/learn/substring/BoyerMoore.java
@@ -82,7 +82,7 @@ public class BoyerMoore {
 
 
     /**
-     * Returns the index of the first occurrrence of the pattern string
+     * Returns the index of the first occurrence of the pattern string
      * in the text string.
      *
      * @param  text the text string
