b196d6dd53fd2fe54ab67feb02d3443f015e0775
Partha Mishra <partha.mishra@gmail.com>
2020-11-28 20:06:44 -0600

    Sliding window maximum - using PQ - still TLE

--- algods/src/main/java/com/algods/leetcode/_explore/SlidingWindowMaximum.java
+++ algods/src/main/java/com/algods/leetcode/_explore/SlidingWindowMaximum.java
@@ -48,26 +48,54 @@ Constraints:
 * The queue size need not be the same as the window’s size.
 * Remove redundant elements and the queue should store only elements that need to be considered.
  */
+import java.util.*;
 public class SlidingWindowMaximum {
-	/* Approach 01: Brute Force
-	 * O(n2) - check each window and calculate max
+	/* REF: 
+	 * https://leetcode.com/problems/sliding-window-maximum/discuss/458121/Java-All-Solutions-(B-F-PQ-Deque-DP)-with-Explanation-and-Complexity-Analysis
+	 * 
+	 */
+	/* Approach 02: Priority Queue : Still TLE as PQ.remove() is O(n)
+	 * O(nk), O(k) for space for the PQ
 	 */
 	public int[] maxSlidingWindow(int[] nums, int k) {
-        int n=nums.length;
-        if(k==0 || n <2) return nums;
-        int r=n-k+1;
-        int[] result=new int[r];
-        for(int i=0; i<= n-k; i++) {
-        	int max=nums[i];
-        	for(int j=1; j< k; j++) {
-        		max=Math.max(max, nums[i+j]);
-        	}
-        	result[i]=max;
-        }
-        return result;
-    }
+		int n=nums.length;
+		if(k==0 || n <2) return nums;
+		int r=n-k+1;
+		int[] result=new int[r];
+		// keeping indices in PQ while the comparison is based on the numbers.
+		// since numbers can be duplicates
+		PriorityQueue<Integer> pq= new PriorityQueue<>((i1,i2) -> Integer.compare(nums[i2], nums[i1]));
+		for(int i=0; i< n; i++) {
+			pq.offer(i);
+			if(i >= k-1) {// for k=3. result starts at i=0,1,2 (k-1) 
+				result[i-k+1]=nums[pq.peek()];// i-k+1= 2-3+1=0 result[0]=max
+				pq.remove(i-k+1);// remove index 0
+			}
+		}
+		return result;
+	}
+	/* Approach 01: Brute Force TLE
+	 * O(nk), O(1) - check each window and calculate max
+	 */
+//	public int[] maxSlidingWindow(int[] nums, int k) {
+//        int n=nums.length;
+//        if(k==0 || n <2) return nums;
+//        int r=n-k+1;
+//        int[] result=new int[r];
+//        for(int i=0; i<= n-k; i++) {
+//        	int max=nums[i];
+//        	for(int j=1; j< k; j++) {
+//        		max=Math.max(max, nums[i+j]);
+//        	}
+//        	result[i]=max;
+//        }
+//        return result;
+//    }
 	public static void main(String[] args) {
 		SlidingWindowMaximum instance= new SlidingWindowMaximum();
-		instance.maxSlidingWindow(new int[] {1,3,-1,-3,5,3,6,7}, 3);
+		int[] result=instance.maxSlidingWindow(new int[] {1,3,-1,-3,5,3,6,7}, 3);
+		for(int r: result) {
+			System.out.print(r+" ");
+		}
 	}
 }
