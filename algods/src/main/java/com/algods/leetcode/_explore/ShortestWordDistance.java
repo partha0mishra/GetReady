2e92b32753a197ba670a02861b09a29e975f0ed7
Partha Mishra <partha.mishra@gmail.com>
2020-12-01 16:06:29 -0600

    Shortest Word Distance - easy

---
+++ algods/src/main/java/com/algods/leetcode/_explore/ShortestWordDistance.java
@@ -0,0 +1,33 @@
+package com.algods.leetcode._explore;
+/*
+ * Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+Example:
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Input: word1 = “coding”, word2 = “practice”
+Output: 3
+Input: word1 = "makes", word2 = "coding"
+Output: 1
+Note:
+You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+ */
+import java.util.*;
+public class ShortestWordDistance {
+	public int shortestDistance(String[] words, String word1, String word2) {
+		List<Integer> w1index= new ArrayList<>(), w2index= new ArrayList<>();
+		for(int i=0; i< words.length; i++) {
+			if(words[i].equals(word1)) w1index.add(i);
+			else if(words[i].equals(word2)) w2index.add(i);
+		}
+		Collections.sort(w1index);
+		Collections.sort(w2index);
+		int dist=Integer.MAX_VALUE;
+		for(int i=0; i< w1index.size(); i++)
+			for(int j=0; j< w2index.size(); j++) {
+				int d=Math.abs(w1index.get(i) - w2index.get(i))+1;
+				if(d < dist) dist=d;
+			}
+		return dist;
+    }
+}
