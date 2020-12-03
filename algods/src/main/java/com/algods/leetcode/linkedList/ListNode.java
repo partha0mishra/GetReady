169c2754c0b5149784d8a737148086eb465bb1e2
Mishra <partha.mishra@gmail.com>
2020-12-02 20:43:05 -0600

    LL Swap Nodes in pairs - good recursion O(n) O(n)

--- algods/src/main/java/com/algods/leetcode/linkedList/ListNode.java
+++ algods/src/main/java/com/algods/leetcode/linkedList/ListNode.java
@@ -5,7 +5,7 @@ package com.algods.leetcode.linkedList;
  */
 public class ListNode {
      int val;
-     ListNode next;
+     public ListNode next;
      ListNode() {}
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next;}
