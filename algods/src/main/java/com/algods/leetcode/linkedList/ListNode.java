85be67a1447be70f44a0772cd81bba974747b863
Mishra <partha.mishra@gmail.com>
2020-12-02 20:54:57 -0600

    LL Swap Nodes in pairs - Recursion -> Iterative. Using Dummy Node. O(n) O(1)

--- algods/src/main/java/com/algods/leetcode/linkedList/ListNode.java
+++ algods/src/main/java/com/algods/leetcode/linkedList/ListNode.java
@@ -7,6 +7,6 @@ public class ListNode {
      int val;
      public ListNode next;
      ListNode() {}
-     ListNode(int val) { this.val = val; }
+     public ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next;}
 }
