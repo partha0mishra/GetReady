935da5627d4a6582fd1ef2dea88827f0eca6b320
Mishra <partha.mishra@gmail.com>
2020-12-03 22:32:34 -0600

    Merge Sorted LL - Iterative O(n+m) O(1)

--- algods/src/main/java/com/algods/leetcode/recursion/MergeSortedLinkedLists.java
+++ algods/src/main/java/com/algods/leetcode/recursion/MergeSortedLinkedLists.java
@@ -19,21 +19,34 @@ public class MergeSortedLinkedLists {
 	     ListNode(int val) { this.val = val; }
 	     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 	 }
-	 /* Approach 02: recursive O(n) O(n) */
+	 /* Approach 03: Iterative O(m+n) O(1)*/
 	 public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-	        if(l1 == null) return l2;
-	        if(l2 == null) return l1;
-	        ListNode head;
-	        if(l1.val < l2.val) {
-	            head= l1;
-	            l1=l1.next;
-	        }else{
-	            head=l2;
-	            l2=l2.next;
-	        }
-	        head.next=mergeTwoLists(l1,l2);
-	        return head;
-	    }
+		 ListNode dummy= new ListNode(-1);
+		 ListNode current=dummy;
+		 while(l1 != null || l2 != null) {
+			 if(l1 == null) {current.next=l2; break;}
+			 if(l2 == null) {current.next=l1; break;}
+			 if(l1.val < l2.val) {current.next=l1; l1=l1.next;}
+			 else {current.next = l2; l2=l2.next;}
+			 current=current.next;
+		 }
+		 return dummy.next;
+	 }
+	 /* Approach 02: recursive O(m+n) O(m+n) */
+//	 public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//	        if(l1 == null) return l2;
+//	        if(l2 == null) return l1;
+//	        ListNode head;
+//	        if(l1.val < l2.val) {
+//	            head= l1;
+//	            l1=l1.next;
+//	        }else{
+//	            head=l2;
+//	            l2=l2.next;
+//	        }
+//	        head.next=mergeTwoLists(l1,l2);
+//	        return head;
+//	    }
 	 /* Approach 01: Brute*/
 //	 public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 //		 if(l1 == null) return l2;
