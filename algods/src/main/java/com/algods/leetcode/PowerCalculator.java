9d18d2593e09bdb2604a0347a96a2e2154e4d9d0
Mishra <partha.mishra@gmail.com>
2020-12-03 19:50:26 -0600

    power(x,n) - recursive - stack overflow

--- algods/src/main/java/com/algods/leetcode/PowerCalculator.java
+++ algods/src/main/java/com/algods/leetcode/PowerCalculator.java
@@ -16,43 +16,49 @@ package com.algods.leetcode;
  * n is a 32-bit signed integer, within the range [−231, 231 − 1]
  */
 
-
+import static org.junit.Assert.assertEquals;
 public class PowerCalculator {
-
+	/* Recursion: stack overflow :D */
 	public double myPow(double x, int n) {
-//		System.out.println(x+" ^ "+n);
-		double result = 1.00000;
-		boolean negative = false;
-		
-		if(n <0) {
-			negative = true;
-			n= -n;
-		}
-		
-		result=power(x,n);
-		if(negative) {
-			result = 1/result;
-		}
-		return result;
-    }
-	
-	private double power(double a, int n) {
-//		System.out.println(a+" ^ "+n);
-		if(n ==0 || a==1) return 1;// a^0 = 1, 1^n = 1
-		double x=power(a,n/2);
-		
-		if(n%2 == 0) return x*x;
-		else return a*x*x;
+		if(x == 1 || n==0) return 1;
+		if(n <0 ) {x= 1/x; n=-n;}
+		return x*myPow(x,n-1);
 	}
+	/* Approach 01: somehow O(n) O(n)*/
+//	public double myPow(double x, int n) {
+////		System.out.println(x+" ^ "+n);
+//		double result = 1.00000;
+//		boolean negative = false;
+//		
+//		if(n <0) {
+//			negative = true;
+//			n= -n;
+//		}
+//		
+//		result=power(x,n);
+//		if(negative) {
+//			result = 1/result;
+//		}
+//		return result;
+//    }
+//	
+//	private double power(double a, int n) {
+////		System.out.println(a+" ^ "+n);
+//		if(n ==0 || a==1) return 1;// a^0 = 1, 1^n = 1
+//		double x=power(a,n/2);
+//		
+//		if(n%2 == 0) return x*x;
+//		else return a*x*x;
+//	}
 	public static void main(String[] args) {
-		System.out.println(2.00000+" ^ "+10+" = "+new PowerCalculator().myPow(2.00000, 10));//1024
-		System.out.println(2.10000+" ^ "+3+" = "+new PowerCalculator().myPow(2.10000, 3));//9.26100
-		System.out.println(2.00000+" ^ "+-2+" = "+new PowerCalculator().myPow(2.00000, -2));//0.25000
-		System.out.println(2.00000+" ^ "+3+" = "+new PowerCalculator().myPow(2.00000, 3));//8.0000
+//		System.out.println(2.00000+" ^ "+10+" = "+new PowerCalculator().myPow(2.00000, 10));//1024
+//		System.out.println(2.10000+" ^ "+3+" = "+new PowerCalculator().myPow(2.10000, 3));//9.26100
+//		System.out.println(2.00000+" ^ "+-2+" = "+new PowerCalculator().myPow(2.00000, -2));//0.25000
+//		System.out.println(2.00000+" ^ "+3+" = "+new PowerCalculator().myPow(2.00000, 3));//8.0000
 		System.out.println(1.0E-5+" ^ "+2147483647+" = "+new PowerCalculator().myPow(1.0E-5, 2147483647));//0.00000
-		System.out.println(2.00000+" ^ "+-2147483648+" = "+new PowerCalculator().myPow(2.00000, -2147483648));//0.00000
-		System.out.println(1.00000+" ^ "+-2147483648+" = "+new PowerCalculator().myPow(1.00000, -2147483648));//1.00000
-		System.out.println(-1.00000+" ^ "+-2147483648+" = "+new PowerCalculator().myPow(-1.00000, -2147483648));//1.00000
+//		System.out.println(2.00000+" ^ "+-2147483648+" = "+new PowerCalculator().myPow(2.00000, -2147483648));//0.00000
+//		System.out.println(1.00000+" ^ "+-2147483648+" = "+new PowerCalculator().myPow(1.00000, -2147483648));//1.00000
+//		System.out.println(-1.00000+" ^ "+-2147483648+" = "+new PowerCalculator().myPow(-1.00000, -2147483648));//1.00000
 	}
 
 }
