294bae4f6aed3ffbf90d235525309203924a9d30
Mishra <partha.mishra@gmail.com>
2020-12-10 20:16:52 -0600

    Morris in-order traversal using Threaded binary tree - O(n) O(n)

--- algods/src/main/java/com/algods/leetcode/bst/BinaryTreeInOrderTraversal.java
+++ algods/src/main/java/com/algods/leetcode/bst/BinaryTreeInOrderTraversal.java
@@ -41,23 +41,61 @@ Recursive solution is trivial, could you do it iteratively?
  */
 import java.util.*;
 public class BinaryTreeInOrderTraversal {
+	/* Morris In-order traversal using Threaded Binary Tree O(n) O(n)
+	 * Step 1: Initialize current as root
+
+Step 2: While current is not NULL,
+
+If current does not have left child
+
+    a. Add current’s value
+
+    b. Go to the right, i.e., current = current.right
+
+Else
+
+    a. In current's left subtree, make current the right child of the rightmost node
+
+    b. Go to this left child, i.e., current = current.left
+	 * */
+	public List < Integer > inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        TreeNode curr = root;
+        TreeNode pre;
+        while (curr != null) {
+            if (curr.left == null) {
+                res.add(curr.val);
+                curr = curr.right; // move to next right node
+            } else { // has a left subtree
+                pre = curr.left;
+                while (pre.right != null) { // find rightmost
+                    pre = pre.right;
+                }
+                pre.right = curr; // put cur after the pre node
+                TreeNode temp = curr; // store cur node
+                curr = curr.left; // move cur to the top of the new tree
+                temp.left = null; // original cur left be null, avoid infinite loops
+            }
+        }
+        return res;
+    }
 	/* Iterative with Stack 
 	 * O(n) O(n)*/
-	 public List < Integer > inorderTraversal(TreeNode root) {
-	        List < Integer > res = new ArrayList < > ();
-	        Stack < TreeNode > stack = new Stack < > ();
-	        TreeNode curr = root;
-	        while (curr != null || !stack.isEmpty()) {
-	            while (curr != null) {
-	                stack.push(curr);
-	                curr = curr.left;
-	            }
-	            curr = stack.pop();
-	            res.add(curr.val);
-	            curr = curr.right;
-	        }
-	        return res;
-	    }
+//	 public List < Integer > inorderTraversal(TreeNode root) {
+//	        List < Integer > res = new ArrayList < > ();
+//	        Stack < TreeNode > stack = new Stack < > ();
+//	        TreeNode curr = root;
+//	        while (curr != null || !stack.isEmpty()) {
+//	            while (curr != null) {
+//	                stack.push(curr);
+//	                curr = curr.left;
+//	            }
+//	            curr = stack.pop();
+//	            res.add(curr.val);
+//	            curr = curr.right;
+//	        }
+//	        return res;
+//	    }
 	/* Recursive O(n) O(n) worst case */
 //	public List<Integer> inorderTraversal(TreeNode root) {
 //        List<Integer> result=new ArrayList<>();
