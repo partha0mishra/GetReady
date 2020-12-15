59706fb7dae06703039465a657b727d4999b93c1
Mishra <partha.mishra@gmail.com>
2020-12-15 13:55:18 -0600

    Plus One Linked List - iterative - O(N) O(1)

--- algods/src/main/java/com/algods/leetcode/_explore/PlusOneLinkedList.java
+++ algods/src/main/java/com/algods/leetcode/_explore/PlusOneLinkedList.java
@@ -26,23 +26,48 @@ public class PlusOneLinkedList {
 	    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 	}
 	public ListNode plusOne(ListNode head) {
-		int carry=addOne(head);
-		if(carry>0) {
-			ListNode newHead=new ListNode(carry);
-			newHead.next=head;
-			head=newHead;
-		}
-		return head;
-    }
-	private int addOne(ListNode node) {
-		if(node==null) {
-			return 1;
-		}
-		node.val+=addOne(node.next);
-		int carry=node.val/10;
-		node.val%=10;
-		return carry;
-	}
+	    // sentinel head
+	    ListNode sentinel = new ListNode(0);
+	    sentinel.next = head;
+	    ListNode notNine = sentinel;
+	    
+	    // find the rightmost not-nine digit
+	    while (head != null) {
+	      if (head.val != 9) notNine = head;
+	      head = head.next;
+	    }
+	    
+	    // increase this rightmost not-nine digit by 1
+	    notNine.val++;
+	    notNine = notNine.next;
+	    
+	    // set all the following nines to zeros
+	    while (notNine != null) {
+	      notNine.val = 0;
+	      notNine = notNine.next;
+	    }
+	    
+	    return sentinel.val != 0 ? sentinel : sentinel.next;
+	  }
+	/* Recursive - O(N) O(N)*/
+//	public ListNode plusOne(ListNode head) {
+//		int carry=addOne(head);
+//		if(carry>0) {
+//			ListNode newHead=new ListNode(carry);
+//			newHead.next=head;
+//			head=newHead;
+//		}
+//		return head;
+//    }
+//	private int addOne(ListNode node) {
+//		if(node==null) {
+//			return 1;
+//		}
+//		node.val+=addOne(node.next);
+//		int carry=node.val/10;
+//		node.val%=10;
+//		return carry;
+//	}
 	public static void main(String[] args) {
 	}
 }
