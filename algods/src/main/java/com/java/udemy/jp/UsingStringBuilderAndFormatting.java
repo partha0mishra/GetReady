48bf854440752706a0d5ce0d2610aefe82367cc9
Mishra <partha.mishra@gmail.com>
2020-12-16 12:03:20 -0600

    .

--- algods/src/main/java/com/java/udemy/jp/UsingStringBuilderAndFormatting.java
+++ algods/src/main/java/com/java/udemy/jp/UsingStringBuilderAndFormatting.java
@@ -16,6 +16,16 @@ public class UsingStringBuilderAndFormatting {
 		sb.append("efficient");
 		sb.append(" using StringBuilder.");
 		System.out.println(sb);
+		
+		/* Formatting */
+		System.out.print("some text \nsome more");// with \n
+		System.out.println(" and some more on the same line.");
+		System.out.printf("Quantity: %-5d Rate: %.2f Total: %-5.2f - %s \n", 10, 0.5,(10*0.5),"looks good");
+		// using                      -  sign to align left
+		// more on floating points printing
+		System.out.printf("% .2f \n", 5.6);
+		System.out.printf("% .2f \n", 5.67);
+		System.out.printf("% .2f \n", 5.6789);
 	}
 
 }
