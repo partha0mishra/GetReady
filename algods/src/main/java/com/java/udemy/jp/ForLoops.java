b25d5d778bb1aad63df5aeee205a7df81c1fc30c
Mishra <partha.mishra@gmail.com>
2020-12-16 10:48:34 -0600

    .

---
+++ algods/src/main/java/com/java/udemy/jp/ForLoops.java
@@ -0,0 +1,9 @@
+package com.java.udemy.jp;
+
+public class ForLoops {
+	public static void main(String[] args) {
+		int end=10;
+		for(int i=0; i< end; i++)
+			System.out.printf("The value of the integer is %d \n", i);
+	}
+}
