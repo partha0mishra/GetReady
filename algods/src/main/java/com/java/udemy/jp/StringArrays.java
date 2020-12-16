b0fa8c84f623158ad987794ad723f0ec2a0e4ad8
Mishra <partha.mishra@gmail.com>
2020-12-16 11:15:36 -0600

    .

---
+++ algods/src/main/java/com/java/udemy/jp/StringArrays.java
@@ -0,0 +1,10 @@
+package com.java.udemy.jp;
+
+public class StringArrays {
+	public static void main(String[] args) {
+		String[] words= {"Hello","to","you"};
+		System.out.println(words[2]);
+		for(String word: words)
+			System.out.println(word);
+	}
+}
