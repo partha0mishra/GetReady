8a8f32955b39001577885f75dfb6e5c7c6c78fa6
Mishra <partha.mishra@gmail.com>
2020-12-16 10:46:08 -0600

    .

---
+++ algods/src/main/java/com/java/udemy/jp/WhileLoops.java
@@ -0,0 +1,12 @@
+package com.java.udemy.jp;
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+		int value=0;
+		while(value<10) {
+			System.out.println(value++);
+		}
+	}
+
+}
