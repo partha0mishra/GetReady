0acc31ee2a92b4696d977abc09041a6e9f487a90
Mishra <partha.mishra@gmail.com>
2020-12-16 12:05:31 -0600

    .

--- algods/src/main/java/com/java/udemy/jp/UsingStringBuilderAndFormatting.java
+++ algods/src/main/java/com/java/udemy/jp/UsingStringBuilderAndFormatting.java
@@ -23,9 +23,11 @@ public class UsingStringBuilderAndFormatting {
 		System.out.printf("Quantity: %-5d Rate: %.2f Total: %-5.2f - %s \n", 10, 0.5,(10*0.5),"looks good");
 		// using                      -  sign to align left
 		// more on floating points printing
-		System.out.printf("% .2f \n", 5.6);
-		System.out.printf("% .2f \n", 5.67);
-		System.out.printf("% .2f \n", 5.6789);
+		System.out.printf("% .2f \n", 5.6);// 2 decimal points. adds 0 at the end
+		System.out.printf("% .2f \n", 5.67);// 2 decimal points
+		System.out.printf("% .2f \n", 5.6789);// 2 decimal points. rounded
+		System.out.printf("% 6.2f \n", 123.5678);// 6= total width of displayed characters including '.'
+		System.out.printf("% -6.2f \n", 123.5678);// - for left alignment
 	}
 
 }
