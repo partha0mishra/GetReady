70647316bf1e4c1fd8b27f7fcbe3f2511b92908c
Mishra <partha.mishra@gmail.com>
2020-12-16 11:38:01 -0600

    .

--- algods/src/main/java/com/java/udemy/jp/ClassAndObjects.java
+++ algods/src/main/java/com/java/udemy/jp/ClassAndObjects.java
@@ -1,22 +1,37 @@
 package com.java.udemy.jp;
 
-class Person{
+class Person {
 	String name;
 	int age;
+
 	public Person(String n, int a) {
-		this.name=n;
-		this.age=a;
+		this.name = n;
+		this.age = a;
 	}
+
 	String getName() {
 		return this.name;
 	}
+
 	int getAge() {
 		return this.age;
 	}
+	
+	void tellName() {
+		System.out.println("Hallo, ich bin "+name+" !!");
+	}
 }
+
 public class ClassAndObjects {
 	public static void main(String[] args) {
-		Person jeff=new Person("Jeff",30);
-		System.out.println("Name: "+jeff.getName()+", Age: "+jeff.getAge());
+		Person jeff = new Person("Jeff", 30);
+		Person sarah = new Person("Sarah", 40);
+		Person[] people = new Person[2];
+		people[0] = jeff;
+		people[1] = sarah;
+		for (int i = 0; i < people.length; i++) {
+			System.out.println("Name: " + people[i].getName() + ", Age: " + people[i].getAge());
+			people[i].tellName();
+		}
 	}
 }
