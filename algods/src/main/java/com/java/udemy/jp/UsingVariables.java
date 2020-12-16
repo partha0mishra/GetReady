b068ed6e303b5f0a8ddc24fc50abeb8207b0dadf
Mishra <partha.mishra@gmail.com>
2020-12-16 10:39:36 -0600

    .

---
+++ algods/src/main/java/com/java/udemy/jp/UsingVariables.java
@@ -0,0 +1,28 @@
+package com.java.udemy.jp;
+
+public class UsingVariables {
+	public static void main(String[] args) {
+		int myNumber;// declaration
+		myNumber=88;// initialization
+		System.out.println(myNumber);
+		
+		short myShort=123;
+		System.out.println(myShort);
+		
+		long myLong=1234567891;
+		System.out.println(myLong);
+		
+		float myFloat=123.45f;
+		System.out.println(myFloat);
+		
+		char myChar='y';
+		System.out.println(myChar);
+		
+		boolean myBool=true;
+		System.out.println(myBool);
+		
+		byte myByte=127;
+		System.out.println(myByte);
+	}
+
+}
