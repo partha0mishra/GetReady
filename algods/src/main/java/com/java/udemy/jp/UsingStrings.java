7ed39b650ed19250c624a2ffee5e4d42085cc554
Mishra <partha.mishra@gmail.com>
2020-12-16 10:44:18 -0600

    .

---
+++ algods/src/main/java/com/java/udemy/jp/UsingStrings.java
@@ -0,0 +1,14 @@
+package com.java.udemy.jp;
+
+public class UsingStrings {
+	public static void main(String[] args) {
+		int myInt=7;
+		String text="Hello";
+		String blank=" ";
+		String name="Bob";
+		String greeting=text+blank+name;
+		System.out.println(text);
+		System.out.println(greeting);
+		System.out.println("My integer is: "+myInt);
+	}
+}
