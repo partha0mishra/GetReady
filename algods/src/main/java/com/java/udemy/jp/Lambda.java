6ba636f58cc505e53e711c7cf521a767b124fabe
Mishra <partha.mishra@gmail.com>
2020-12-17 10:59:32 -0600

    .

--- algods/src/main/java/com/java/udemy/jp/Lambda.java
+++ algods/src/main/java/com/java/udemy/jp/Lambda.java
@@ -9,6 +9,18 @@ class Runner{
 		e.execute();
 	}
 }
+
+interface Exec2{
+	public int exec();
+}
+
+class Runner2{
+	public void run(Exec2 e) {
+		System.out.println("new runner run ");
+		int value=e.exec();
+		System.out.println("Returned value: "+value);
+	}
+}
 // () -> System.out.println("hello there")
 /*
  * () -> {
@@ -33,6 +45,11 @@ public class Lambda {
 		runner.run(() -> {
 			System.out.println("putting multiple statements");
 			System.out.println("hello there");
+		});
+		System.out.println("== returning a value ==");
+		new Runner2().run(() -> {
+			System.out.println(">> Lambda returning value");
+			return 42;
 		});
 	}
 }
