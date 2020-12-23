7398cfa140d0182154a55f994c81bfaf35887eee
Mishra <partha.mishra@gmail.com>
2020-12-22 20:34:41 -0600

    .

--- algods/src/main/java/com/algods/leetcode/backtracking/GenerateParentheses.java
+++ algods/src/main/java/com/algods/leetcode/backtracking/GenerateParentheses.java
@@ -1,4 +1,5 @@
 package com.algods.leetcode.backtracking;
+// TODO Anki
 /* 22. Generate Parentheses
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 Example 1:
--- algods/src/main/java/com/algods/leetcode/bst/BinaryTreeInOrderTraversal.java
+++ algods/src/main/java/com/algods/leetcode/bst/BinaryTreeInOrderTraversal.java
@@ -1,4 +1,5 @@
 package com.algods.leetcode.bst;
+// TODO Anki
 /* 94. Binary Tree InOrder Traversal
  * Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
@@ -41,44 +42,37 @@ Recursive solution is trivial, could you do it iteratively?
  */
 import java.util.*;
 public class BinaryTreeInOrderTraversal {
-	/* Morris In-order traversal using Threaded Binary Tree O(n) O(n)
+	/* Morris In-order traversal using Threaded Binary Tree O(n) O(1)
 	 * Step 1: Initialize current as root
-
-Step 2: While current is not NULL,
-
-If current does not have left child
-
-    a. Add current’s value
-
-    b. Go to the right, i.e., current = current.right
-
-Else
-
-    a. In current's left subtree, make current the right child of the rightmost node
-
-    b. Go to this left child, i.e., current = current.left
+	 * Step 2: While current is not NULL,
+	 * If current does not have left child
+	 * a. Add current’s value
+	 * b. Go to the right, i.e., current = current.right
+	 * Else
+	 * a. In current's left subtree, make current the right child of the rightmost node
+	 * b. Go to this left child, i.e., current = current.left
 	 * */
-	public List < Integer > inorderTraversal(TreeNode root) {
-        List < Integer > res = new ArrayList < > ();
-        TreeNode curr = root;
-        TreeNode pre;
-        while (curr != null) {
-            if (curr.left == null) {
-                res.add(curr.val);
-                curr = curr.right; // move to next right node
-            } else { // has a left subtree
-                pre = curr.left;
-                while (pre.right != null) { // find rightmost
-                    pre = pre.right;
-                }
-                pre.right = curr; // put cur after the pre node
-                TreeNode temp = curr; // store cur node
-                curr = curr.left; // move cur to the top of the new tree
-                temp.left = null; // original cur left be null, avoid infinite loops
-            }
-        }
-        return res;
-    }
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<>();
+		TreeNode curr = root;
+		TreeNode pre;
+		while (curr != null) {
+			if (curr.left == null) {
+				res.add(curr.val);
+				curr = curr.right; // move to next right node
+			} else { // has a left subtree
+				pre = curr.left;
+				while (pre.right != null) { // find rightmost
+					pre = pre.right;
+				}
+				pre.right = curr; // put cur after the pre node
+				TreeNode temp = curr; // store cur node
+				curr = curr.left; // move cur to the top of the new tree
+				temp.left = null; // original cur left needs to be null, avoid infinite loops
+			}
+		}
+		return res;
+	}
 	/* Iterative with Stack 
 	 * O(n) O(n)*/
 //	 public List < Integer > inorderTraversal(TreeNode root) {
