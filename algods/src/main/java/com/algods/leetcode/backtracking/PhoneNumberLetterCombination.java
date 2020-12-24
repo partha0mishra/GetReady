29680b85bea8fb0503749597f164dded36415c4b
Mishra <partha.mishra@gmail.com>
2020-12-23 19:59:42 -0600

    .

--- algods/src/main/java/com/algods/leetcode/backtracking/PhoneNumberLetterCombination.java
+++ algods/src/main/java/com/algods/leetcode/backtracking/PhoneNumberLetterCombination.java
@@ -2,13 +2,7 @@ package com.algods.leetcode.backtracking;
 // TODO Anki
 /* Letter Combinations of a Phone Number
  * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
-
 A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
-
-
-
- 
-
 Example 1:
 
 Input: digits = "23"
