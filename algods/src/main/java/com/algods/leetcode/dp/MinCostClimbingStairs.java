cf6fc95fd6d89213386a8aca12f95dc6d053682a
Mishra <partha.mishra@gmail.com>
2020-12-24 12:00:21 -0600

    .

--- algods/src/main/java/com/algods/leetcode/dp/MinCostClimbingStairs.java
+++ algods/src/main/java/com/algods/leetcode/dp/MinCostClimbingStairs.java
@@ -19,28 +19,41 @@ Note:
 cost will have a length in the range [2, 1000].
 Every cost[i] will be an integer in the range [0, 999].
  */
+import static org.junit.Assert.assertEquals;
 public class MinCostClimbingStairs {
 	/* Recurrence relationship:
 	 * minCost(i)=min(minCost(i-1) + cost(i-1), minCost(i-2)+cost[i-2])
-	 * O(N)/ O(N)
+	 * 
+	 * O(N)/ O(1)
 	 */
-	int[] dp;
 	public int minCostClimbingStairs(int[] cost) {
-        int n=cost.length;
-        dp=new int[n];
-        dp[0]=cost[0];
-        dp[1]=cost[1];
-        for(int i=2; i< n; i++) {
-        	dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i];
-        }
-        return Math.min(dp[n-1], dp[n-2]);
-    }
+		int prev2=cost[0], prev1=cost[1];
+		for(int i=2; i< cost.length; i++) {
+			int current=Math.min(prev2, prev1)+cost[i];
+			prev2=prev1;
+			prev1=current;
+		}
+		return Math.min(prev1, prev2);
+	}
+	 /* O(N)/ O(N)
+	 */
+//	int[] dp;
+//	public int minCostClimbingStairs(int[] cost) {
+//        int n=cost.length;
+//        dp=new int[n];
+//        dp[0]=cost[0];
+//        dp[1]=cost[1];
+//        for(int i=2; i< n; i++) {
+//        	dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i];
+//        }
+//        return Math.min(dp[n-1], dp[n-2]);
+//    }
 	public static void main(String[] args) {
 		MinCostClimbingStairs mccs= new MinCostClimbingStairs();
-		System.out.println(mccs.minCostClimbingStairs(new int[] {10,15}));
-		System.out.println(mccs.minCostClimbingStairs(new int[] {10,8}));
-		System.out.println(mccs.minCostClimbingStairs(new int[] {10,15,20}));
-		System.out.println(mccs.minCostClimbingStairs(new int[] {1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
+		assertEquals(10,(mccs.minCostClimbingStairs(new int[] {10,15})));
+		assertEquals(8,(mccs.minCostClimbingStairs(new int[] {10,8})));
+		assertEquals(15,(mccs.minCostClimbingStairs(new int[] {10,15,20})));
+		assertEquals(6,(mccs.minCostClimbingStairs(new int[] {1, 100, 1, 1, 1, 100, 1, 1, 100, 1})));
 	}
 
 }
