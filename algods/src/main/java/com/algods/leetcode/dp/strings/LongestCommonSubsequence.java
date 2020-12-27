23a1213e6c6fff97157db839cd4307f0ee981b38
Mishra <partha.mishra@gmail.com>
2020-12-26 21:20:09 -0600

    .

--- algods/src/main/java/com/algods/leetcode/dp/strings/LongestCommonSubsequence.java
+++ algods/src/main/java/com/algods/leetcode/dp/strings/LongestCommonSubsequence.java
@@ -163,27 +163,58 @@ public class LongestCommonSubsequence {
 	 * 
 	 * O(MN)/ O(MN)
 	 */
+//	public int longestCommonSubsequence(String text1, String text2) {    
+//
+//		// Make a grid of 0's with text2.length() + 1 columns 
+//		// and text1.length() + 1 rows.
+//		int[][] dpGrid = new int[text1.length() + 1][text2.length() + 1];
+//
+//		// Iterate up each column, starting from the last one.
+//		for (int col = text2.length() - 1; col >= 0; col--) {
+//			for (int row = text1.length() - 1; row >= 0; row--) {
+//				// If the corresponding characters for this cell are the same...
+//				if (text1.charAt(row) == text2.charAt(col)) {
+//					dpGrid[row][col] = 1 + dpGrid[row + 1][col + 1];
+//					// Otherwise they must be different...
+//				} else {
+//					dpGrid[row][col] = Math.max(dpGrid[row + 1][col], dpGrid[row][col + 1]);
+//				}
+//			}
+//		}
+//
+//		// The original problem's answer is in dp_grid[0][0]. Return it.
+//		return dpGrid[0][0];
+//	}
+	/**
+	 * [Approach 04]
+	 * Since we only need to keep the current column and the last column
+	 * We can improve O(M) to be O(Min(M,N))
+	 */
 	public int longestCommonSubsequence(String text1, String text2) {    
+		// If text1 doesn't reference the shortest string, swap them.
+		if (text2.length() < text1.length()) {String temp = text1;text1 = text2;text2 = temp;}
 
-		// Make a grid of 0's with text2.length() + 1 columns 
-		// and text1.length() + 1 rows.
-		int[][] dpGrid = new int[text1.length() + 1][text2.length() + 1];
+		// The previous column starts with all 0's and like before is 1
+		// more than the length of the first word.
+		int[] previous = new int[text1.length() + 1];
 
-		// Iterate up each column, starting from the last one.
+		// Iterate through each column, starting from the last one.
 		for (int col = text2.length() - 1; col >= 0; col--) {
+			// Create a new array to represent the current column.
+			int[] current = new int[text1.length() + 1];  
 			for (int row = text1.length() - 1; row >= 0; row--) {
-				// If the corresponding characters for this cell are the same...
 				if (text1.charAt(row) == text2.charAt(col)) {
-					dpGrid[row][col] = 1 + dpGrid[row + 1][col + 1];
-					// Otherwise they must be different...
+					current[row] = 1 + previous[row + 1];
 				} else {
-					dpGrid[row][col] = Math.max(dpGrid[row + 1][col], dpGrid[row][col + 1]);
+					current[row] = Math.max(previous[row], current[row + 1]);
 				}
 			}
+			// The current column becomes the previous one.
+			previous = current;
 		}
 
-		// The original problem's answer is in dp_grid[0][0]. Return it.
-		return dpGrid[0][0];
+		// The original problem's answer is in previous[0]. Return it.
+		return previous[0];
 	}
 	/**
 	 * [Approach 05]
