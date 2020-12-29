2f8c40cbfd767a8723576ef0e24cf43a37ddeb70
Mishra <partha.mishra@gmail.com>
2020-12-28 20:34:09 -0600

    adding changes

--- algods/src/main/java/com/leetcode/bst/SearchInBST.java
+++ algods/src/main/java/com/leetcode/bst/SearchInBST.java
@@ -1,4 +1,5 @@
 package com.leetcode.bst;
+// TODO Anki
 /* 700. Search in a Binary Search Tree
  * Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
 
