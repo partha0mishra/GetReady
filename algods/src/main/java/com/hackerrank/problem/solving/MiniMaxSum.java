704a01b97d34b03940a1c53484cb54bed45b683e
Mishra <partha.mishra@gmail.com>
2020-12-28 19:32:32 -0600

    adding changes

---
+++ algods/src/main/java/com/hackerrank/problem/solving/MiniMaxSum.java
@@ -0,0 +1,55 @@
+package com.hackerrank.problem.solving;
+
+import java.util.Arrays;
+
+/**
+ * Mini Max Sum
+ * Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+Print
+
+Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+Sample Output
+
+10 14
+
+Hints: Beware of integer overflow! Use 64-bit Integer.
+ */
+public class MiniMaxSum {
+	static void miniMaxSum(int[] arr) {
+        Arrays.sort(arr);
+        long min=0, max=0;
+        for(int i=0; i< arr.length; i++){
+            if(i != arr.length-1) min+=arr[i];
+            if(i != 0) max+=arr[i];
+        }
+        System.out.println(min+" "+max);
+    }
+}
