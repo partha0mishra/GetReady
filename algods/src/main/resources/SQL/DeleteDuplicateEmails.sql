a5b2cb70b8f2cb13078e23c6e530074b4b7d2987
Mishra <partha.mishra@gmail.com>
2020-12-29 17:15:18 -0600

    adding changes

---
+++ algods/src/main/resources/SQL/DeleteDuplicateEmails.sql
@@ -0,0 +1,27 @@
+-- TODO Anki
+-- Delete Duplicate Emails
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Note:
+
+Your output is the whole Person table after executing your sql. Use delete statement.
+--
+-- Note: need an intermediate select * as we can't just select and update the same table
+delete from Person where id not in
+(select * from (select min(id) from person group by email) p);
