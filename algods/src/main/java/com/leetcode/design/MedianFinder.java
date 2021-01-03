96d705335c0878a8629a6c6cb02919ec2a64c832
Mishra <partha.mishra@gmail.com>
2021-01-02 22:25:46 -0600

    .

--- algods/src/main/java/com/leetcode/design/MedianFinder.java
+++ algods/src/main/java/com/leetcode/design/MedianFinder.java
@@ -39,53 +39,68 @@ public class MedianFinder {
         n+=1;
     }
     
-//    public double findMedian() {
-//    	int cumu=0, pFreq=0, cFreq=0, prevNum=0, currentNum=0;
-//    	for(int key: numFrequencies.keySet()) {
-//    		currentNum=key; cFreq=numFrequencies.get(key);
-//    		if(pFreq == 0) {prevNum=key; pFreq=cFreq;}
-//    		cumu+=cFreq;
-//    		if(cumu > n/2) break;
-//    	}
-//    	// odd: send out (n/2+1)th entry.
-//    	// even: send out 
-//    	double median=currentNum;// has to be the (n/2 +1)th entry
-//    	if(n%2 ==0) {
-//    		//if(cumu - counts[i] < n/2) - then it's the current number once more and then avg. better not to do anything
-//    		if(cumu - cFreq== n/2) median=(median+prevNum)/2;
-//    	}
-//    	return median;
-//    }
-    /*
-     * Approach 01: keeping counts and calculating cumulatives : TLE
+    /**
+     * Approach 02: keeping only previous num is enough
      */
     public double findMedian() {
-    	int[] nums=new int[numFrequencies.size()+1];
-    	int[] counts=new int[numFrequencies.size()+1];
-    	int i=0, cumu=0;
+    	int cumu=0, pFreq=0, cFreq=0, prevNum=0, currentNum=0;
     	for(int key: numFrequencies.keySet()) {
-    		nums[i]=key;
-    		counts[i]=numFrequencies.get(key);
-    		cumu+=counts[i];
+    		currentNum=key; cFreq=numFrequencies.get(key);
+    		if(pFreq == 0) {prevNum=currentNum; pFreq=cFreq;}
+    		cumu+=cFreq;
     		if(cumu > n/2) break;
-    		i++;
+    		prevNum=currentNum; pFreq=cFreq;
     	}
     	// odd: send out (n/2+1)th entry.
     	// even: send out 
-    	double median=nums[i];// has to be the (n/2 +1)th entry
+    	double median=currentNum;// has to be the (n/2 +1)th entry
     	if(n%2 ==0) {
-    		//if(cumu - counts[i] < n/2) - then it's the current number once more and then avg. better not to do anything
-    		if(cumu - counts[i] == n/2) median=(median+nums[i-1])/2;
+    		if(cumu - cFreq== n/2) median=(median+prevNum)/2;
     	}
     	return median;
     }
+    /*
+     * Approach 01: keeping counts and calculating cumulatives : TLE
+     */
+//    public double findMedian() {
+//    	int[] nums=new int[numFrequencies.size()+1];
+//    	int[] counts=new int[numFrequencies.size()+1];
+//    	int i=0, cumu=0;
+//    	for(int key: numFrequencies.keySet()) {
+//    		nums[i]=key;
+//    		counts[i]=numFrequencies.get(key);
+//    		cumu+=counts[i];
+//    		if(cumu > n/2) break;
+//    		i++;
+//    	}
+//    	// odd: send out (n/2+1)th entry.
+//    	// even: send out 
+//    	double median=nums[i];// has to be the (n/2 +1)th entry
+//    	if(n%2 ==0) {
+//    		//if(cumu - counts[i] < n/2) - then it's the current number once more and then avg. better not to do anything
+//    		if(cumu - counts[i] == n/2) median=(median+nums[i-1])/2;
+//    	}
+//    	return median;
+//    }
 	public static void main(String[] args) {
 		MedianFinder mf=new MedianFinder();
-		mf.addNum(1);
+//		mf.addNum(1);
+//		System.out.println(mf.findMedian());
+//		mf.addNum(2);
+//		System.out.println(mf.findMedian());
+//		mf.addNum(3);
+//		System.out.println(mf.findMedian());
+//		===
+		mf=new MedianFinder();
+		mf.addNum(-1);
+//		System.out.println(mf.findMedian());
+		mf.addNum(-2);
+//		System.out.println(mf.findMedian());
+		mf.addNum(-3);
+//		System.out.println(mf.findMedian());
+		mf.addNum(-4);
 		System.out.println(mf.findMedian());
-		mf.addNum(2);
-		System.out.println(mf.findMedian());
-		mf.addNum(3);
+		mf.addNum(-5);
 		System.out.println(mf.findMedian());
 	}
 
