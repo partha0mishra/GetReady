468af9cbb37f237764232fb8834f8e08e9c00147
Mishra <partha.mishra@gmail.com>
2021-01-04 14:29:53 -0600

    Max Stack - with ArrayDeque and TreeMap - good exercise

--- algods/src/main/java/com/leetcode/design/MaxStack.java
+++ algods/src/main/java/com/leetcode/design/MaxStack.java
@@ -47,76 +47,137 @@ import java.util.*;
 import static org.junit.Assert.assertEquals;
 public class MaxStack {
 	/**
+	 * Approach 02: 
+	 * ArrayDeque - keep elements
+	 * TreeMap - keep max
+	 */
+	Deque<Integer> stack;
+	TreeMap<Integer, Integer> map;
+	 /** initialize your data structure here. */
+    public MaxStack() {
+    	stack= new ArrayDeque<>();
+    	map= new TreeMap<>();
+    }
+    /** push element into stack */
+    public void push(int x) {
+        stack.offerFirst(x);
+        map.put(x, map.getOrDefault(x, 0)+1);
+    }
+    /** Removes element at Top and returns */
+    public int pop() {
+    	int result=stack.pollFirst();
+    	if(map.get(result) > 1) map.put(result, map.get(result)-1);
+    	else map.remove(result);
+    	return result;
+    }
+    /** Gets element at Top. NO Removal */
+    public int top() {
+        return stack.peekFirst();
+    }
+    /** Gets Max element. NO Removal */
+    public int peekMax() {
+        return map.lastKey();
+    }
+    /** Returns Max element. Removes it/ top one if multiple */
+    public int popMax() {
+        int max=map.lastKey();
+        if(map.get(max) >1) map.put(max, map.get(max)-1);
+        else map.remove(max);
+        stack.remove(max);
+        return max;
+    }
+	/**
 	 * Approach 01: using ArrayDeque - loaning all the stack operations O(1)
 	 * using Element(val, maxSoFar) to allow quicker peekMax() at O(1)
 	 * O(n) extra space
 	 * popTop() is O(n)
-	 * 
-	 * Alternate could be MaxHeap with logN for all calls and another array with insertion order to get top at O(1) 
 	 */
-	class Element{
-		int val, max;
-		public Element(int v, int m) {this.val=v; this.max=m;}
-	}
-	Deque<Element> stack;
-	 /** initialize your data structure here. */
-    public MaxStack() {
-    	stack= new ArrayDeque<>();
-    }
-    /** push element into stack */
-    public void push(int x) {
-        if(stack.isEmpty()) {
-        	stack.offerFirst(new Element(x,x));
-        }else {
-        	stack.offerFirst(new Element(x, Math.max(x, stack.peekFirst().max)));
-        }
-    }
-    /** Removes element at Top and returns */
-    public int pop() {
-        return stack.pollFirst().val;
-    }
-    /** Gets element at Top. NO Removal */
-    public int top() {
-        return stack.peekFirst().val;
-    }
-    /** Gets Max element. NO Removal */
-    public int peekMax() {
-        return stack.peekFirst().max;
-    }
-    /** Returns Max element. Removes it/ top one if multiple */
-    public int popMax() {
-        int max=peekMax();
-        Deque<Integer> temp= new ArrayDeque<>();
-        while(!stack.isEmpty()) {
-        	int i=stack.pop().val;
-        	if(i == max) {
-        		while(!temp.isEmpty()) {
-        			push(temp.pop());// pushing back the elements
-        		}
-        		break;
-        	}else temp.push(i);
-        }
-        return max;
-    }
+//	class Element{
+//		int val, max;
+//		public Element(int v, int m) {this.val=v; this.max=m;}
+//	}
+//	Deque<Element> stack;
+//	 /** initialize your data structure here. */
+//    public MaxStack() {
+//    	stack= new ArrayDeque<>();
+//    }
+//    /** push element into stack */
+//    public void push(int x) {
+//        if(stack.isEmpty()) {
+//        	stack.offerFirst(new Element(x,x));
+//        }else {
+//        	stack.offerFirst(new Element(x, Math.max(x, stack.peekFirst().max)));
+//        }
+//    }
+//    /** Removes element at Top and returns */
+//    public int pop() {
+//        return stack.pollFirst().val;
+//    }
+//    /** Gets element at Top. NO Removal */
+//    public int top() {
+//        return stack.peekFirst().val;
+//    }
+//    /** Gets Max element. NO Removal */
+//    public int peekMax() {
+//        return stack.peekFirst().max;
+//    }
+//    /** Returns Max element. Removes it/ top one if multiple */
+//    public int popMax() {
+//        int max=peekMax();
+//        Deque<Integer> temp= new ArrayDeque<>();
+//        while(!stack.isEmpty()) {
+//        	int i=stack.pop().val;
+//        	if(i == max) {
+//        		while(!temp.isEmpty()) {
+//        			push(temp.pop());// pushing back the elements
+//        		}
+//        		break;
+//        	}else temp.push(i);
+//        }
+//        return max;
+//    }
 	public static void main(String[] args) {
 		MaxStack ms= new MaxStack();
-//		ms.push(5);
-//		ms.push(1);
-//		ms.push(5);
-//		assertEquals(5,ms.top());// 5
-//		assertEquals(5,ms.popMax());// 5
-//		assertEquals(1,ms.top());// 1
-//		assertEquals(5,ms.peekMax());// 5
-//		assertEquals(1,ms.pop());// 1
-//		assertEquals(5,ms.top());// 5
-		//
+		ms.push(5);
+		ms.push(1);
+		ms.push(5);
+		assertEquals(5,ms.top());// 5
+		assertEquals(5,ms.popMax());// 5
+		assertEquals(1,ms.top());// 1
+		assertEquals(5,ms.peekMax());// 5
+		assertEquals(1,ms.pop());// 1
+		assertEquals(5,ms.top());// 5
+//		/***/
+		ms= new MaxStack();
 		ms.push(5);
 		ms.push(1);
 		ms.push(6);
-		ms.peekMax();//6
-		ms.popMax();//6
-		ms.popMax();//5
-		ms.top();//1
+		assertEquals(6, ms.peekMax());//6
+		assertEquals(6, ms.popMax());//6
+		assertEquals(5, ms.popMax());//5
+		assertEquals(1, ms.top());//1
+		/**
+		 * ["MaxStack","push","popMax","push","push","push","pop","peekMax","push",
+		 * "pop","pop","push","peekMax","peekMax","push","peekMax","push","peekMax"]
+		 * [[],[-2],[],[-45],[-82],[29],[],[],[40],
+		 * [],[],[66],[],[],[-61],[],[98],[]]
+		 * */
+		ms= new MaxStack();
+		ms.push(-2);
+		assertEquals(-2, ms.popMax());
+		ms.push(-45);
+		ms.push(-82);
+		ms.push(29);
+		assertEquals(29, ms.pop());
+		assertEquals(-45,ms.peekMax());
+		ms.push(40);
+		assertEquals(40, ms.pop());
+		assertEquals(-82, ms.pop());
+		ms.push(66);
+		assertEquals(66,ms.peekMax());
+		assertEquals(66,ms.peekMax());
+		ms.push(98);
+		assertEquals(98,ms.peekMax());
 	}
 
 }
