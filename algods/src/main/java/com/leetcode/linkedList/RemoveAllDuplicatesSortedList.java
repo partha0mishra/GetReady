6991784f18a5f853d1245dfdc922a604ad9d4707
Mishra <partha.mishra@gmail.com>
2021-01-05 10:38:53 -0600

    .

--- algods/src/main/java/com/leetcode/linkedList/RemoveAllDuplicatesSortedList.java
+++ algods/src/main/java/com/leetcode/linkedList/RemoveAllDuplicatesSortedList.java
@@ -1,6 +1,6 @@
 package com.leetcode.linkedList;
 
-/**
+/** TODO Anki
  * 82. Remove Duplicates from Sorted List II
  * 
  * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
