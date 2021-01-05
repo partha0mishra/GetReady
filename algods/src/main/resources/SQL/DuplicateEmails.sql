9c5ac095e03cf8f38f988c34fe42d173f35bfc33
Mishra <partha.mishra@gmail.com>
2021-01-05 11:55:55 -0600

    .

---
+++ algods/src/main/resources/SQL/DuplicateEmails.sql
@@ -0,0 +1,22 @@
+-- TODO Anki
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+----
+Select T1.Email from
+(Select Email, Count(*) from Person
+ group by Email
+ having Count(*)>1) T1;
