2bcf57dfcc0ee506353d2215f0162572f3a1c94c
Mishra <partha.mishra@gmail.com>
2021-01-04 18:44:19 -0600

    .

---
+++ algods/src/main/java/com/leetcode/design/PhoneDirectory.java
@@ -0,0 +1,134 @@
+package com.leetcode.design;
+/**
+ * 379. Design Phone Directory
+ * Design a Phone Directory which supports the following operations:
+
+ 
+
+get: Provide a number which is not assigned to anyone.
+check: Check if a number is available or not.
+release: Recycle or release a number.
+ 
+
+Example:
+
+// Init a phone directory containing a total of 3 numbers: 0, 1, and 2.
+PhoneDirectory directory = new PhoneDirectory(3);
+
+// It can return any available phone number. Here we assume it returns 0.
+directory.get();
+
+// Assume it returns 1.
+directory.get();
+
+// The number 2 is available, so return true.
+directory.check(2);
+
+// It returns 2, the only number that is left.
+directory.get();
+
+// The number 2 is no longer available, so return false.
+directory.check(2);
+
+// Release number 2 back to the pool.
+directory.release(2);
+
+// Number 2 is available again, return true.
+directory.check(2);
+ 
+
+Constraints:
+
+1 <= maxNumbers <= 10^4
+0 <= number < maxNumbers
+The total number of call of the methods is between [0 - 20000]
+ */
+import java.util.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+public class PhoneDirectory {
+	/**
+	 * Easy implementation with 2 HashSets - available and used
+	 * constructor: O(n)
+	 * get O(1), check O(1), release O(1)
+	 * extra space O(n)
+	 */
+	Deque<Integer> released;
+	int maxNum, current;
+	/** Initialize your data structure here
+	    @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+	public PhoneDirectory(int maxNumbers) {
+		released=new ArrayDeque<>();// keeping released numbers
+		this.maxNum=maxNumbers;// ending at less than this
+		this.current=0;// starting from this number
+	}
+	
+	/** Provide a number which is not assigned to anyone.
+	    @return - Return an available number. Return -1 if none is available. */
+	public int get() {
+	    if(!released.isEmpty()) return released.pollFirst();// recycle
+	    if(current< maxNum) return current++;// new
+	    return -1;// exhausted
+	}
+	
+	/** Check if a number is available or not. */
+	public boolean check(int number) {
+	    if(!(number < maxNum)) return false;// no going beyond maxNum
+	    if( number < current) return released.contains(number);// if it's used, we can only find it if it's released
+	    return true;// current <= number < maxNum 
+	}
+	
+	/** Recycle or release a number. */
+	public void release(int number) {
+	    released.offerLast(number);
+	}
+	
+//	/**
+//	 * Easy implementation with 2 HashSets - available and used
+//	 * constructor: O(n)
+//	 * get O(1), check O(1), release O(1)
+//	 * extra space O(n)
+//	 */
+//	HashSet<Integer> available;
+//	HashSet<Integer> used;
+//	/** Initialize your data structure here
+//	    @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+//	public PhoneDirectory(int maxNumbers) {
+//		available= new HashSet<>();
+//		used=new HashSet<>();
+//	    for(int i=0; i<maxNumbers; i++) available.add(i);
+//	}
+//	
+//	/** Provide a number which is not assigned to anyone.
+//	    @return - Return an available number. Return -1 if none is available. */
+//	public int get() {
+//	    if(available.size() ==0) return -1;
+//	    int num=available.iterator().next();
+//	    used.add(num);
+//	    available.remove(num);
+//	    return num;
+//	}
+//	
+//	/** Check if a number is available or not. */
+//	public boolean check(int number) {
+//	    return available.contains(number);
+//	}
+//	
+//	/** Recycle or release a number. */
+//	public void release(int number) {
+//	    used.remove(number);
+//	    available.add(number);
+//	}
+
+	/**
+	 * Your PhoneDirectory object will be instantiated and called as such:
+	 * PhoneDirectory obj = new PhoneDirectory(maxNumbers);
+	 * int param_1 = obj.get();
+	 * boolean param_2 = obj.check(number);
+	 * obj.release(number);
+	 */
+	public static void main(String[] args) {
+	}
+
+}
