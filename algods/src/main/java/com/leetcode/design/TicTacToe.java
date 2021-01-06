3d4950df9b351c2fa1c600083b761f3b69d55368
Mishra <partha.mishra@gmail.com>
2021-01-05 20:43:27 -0600

    .

--- algods/src/main/java/com/leetcode/design/TicTacToe.java
+++ algods/src/main/java/com/leetcode/design/TicTacToe.java
@@ -81,10 +81,6 @@ public class TicTacToe {
     }
     
     /** Player {player} makes a move at ({row}, {col}).
-        @param row The row of the board.
-        @param col The column of the board.
-        @param player The player, can be either 1 or 2.
-        @return The current winning condition, can be either:
                 0: No one wins.
                 1: Player 1 wins.
                 2: Player 2 wins. */
