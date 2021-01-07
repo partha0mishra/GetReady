49311f6499ccd3ce3509a47cdb5276f3f2198b90
Mishra <partha.mishra@gmail.com>
2021-01-06 19:57:12 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/MeanAfterRemoving5Percent.java
+++ algods/src/main/java/com/leetcode/arrays/MeanAfterRemoving5Percent.java
@@ -1,8 +1,8 @@
-package com.leetcode.easy;
+package com.leetcode.arrays;
 
 import java.util.Arrays;
 
-/* 5122. Mean of Array After Removing Some Elements
+/* 1619. Mean of Array After Removing Some Elements
  * Given an integer array arr, return the mean of the remaining integers after removing the smallest 5% and the largest 5% of the elements.
 
 Answers within 10-5 of the actual answer will be considered accepted.
