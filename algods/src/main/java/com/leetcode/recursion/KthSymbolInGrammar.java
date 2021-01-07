88982f34af14d94315710cae03edd2e665bc4dce
Mishra <partha.mishra@gmail.com>
2021-01-06 21:24:32 -0600

    .

--- algods/src/main/java/com/leetcode/recursion/KthSymbolInGrammar.java
+++ algods/src/main/java/com/leetcode/recursion/KthSymbolInGrammar.java
@@ -1,5 +1,6 @@
 package com.leetcode.recursion;
-/* 779. K-th Symbol in Grammar
+/* * TODO Anki
+ * 779. K-th Symbol in Grammar
  * On the first row, we write a 0. Now in every subsequent row, we look at the previous row and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
 
 Given row N and index K, return the K-th indexed symbol in row N. (The values of K are 1-indexed.) (1 indexed).
