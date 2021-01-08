26b866f1d059253b19f4ace3f27edace8b9d07d9
Mishra <partha.mishra@gmail.com>
2021-01-07 19:31:46 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/LongestSubstringNoRepeat.java
+++ algods/src/main/java/com/leetcode/arrays/LongestSubstringNoRepeat.java
@@ -37,24 +37,42 @@ import java.util.*;
 import static org.junit.Assert.assertEquals;
 public class LongestSubstringNoRepeat {
 	/**
+	 * Approach 03: Sliding window with more slide - similar to approach 02 but using array[]
+	 * If we keep the index of a character, we can skip till there+1 if we encounter the character again
+	 * O(n)/ O(M)
+	 */
+	public int lengthOfLongestSubstring(String s) {
+        int maxLen=0, n=s.length(),i=0, j=0;
+        if(n ==0) return 0;
+        int[] index=new int[128];// 26 for a-z OR A-Z, 128 for ASCII, 256 for Extended ASCII
+        for(i=0, j=0; i< n;i++) {
+        	if(index[s.charAt(i)] !=0) {
+        		maxLen=Math.max(maxLen, i-j);
+        		j=Math.max(j, index[s.charAt(i)]);
+        	}
+        	index[s.charAt(i)]=i+1;// starting with 1 so that 0 can be the 'default' value
+        }
+        return Math.max(maxLen, i-j);
+    }
+	/**
 	 * Approach 02: Sliding window with more slide
 	 * If we keep the index of a character, we can skip till there+1 if we encounter the character again
 	 * O(n)/ O(Min(M,N)) <= Important as we're going to keep the max substring in temporary variable
 	 */
-	public int lengthOfLongestSubstring(String s) {
-        if(s.length() == 0) return 0;
-        int len=0,i=0, j=0;
-        HashMap<Character,Integer> map=new HashMap<>();// keeping char and index
-        for(i=0, j=0; i< s.length();i++) {
-        	char c=s.charAt(i);
-        	if(map.containsKey(c)) {
-        		len=Math.max(len, i-j);
-        		j=Math.max(j, map.get(c)+1);// abba where first a again matches with last a but we can't decrease j
-        	}
-        	map.put(c, i);
-        }
-        return Math.max(len, i-j);
-    }
+//	public int lengthOfLongestSubstring(String s) {
+//        if(s.length() == 0) return 0;
+//        int len=0,i=0, j=0;
+//        HashMap<Character,Integer> map=new HashMap<>();// keeping char and index
+//        for(i=0, j=0; i< s.length();i++) {
+//        	char c=s.charAt(i);
+//        	if(map.containsKey(c)) {
+//        		len=Math.max(len, i-j);
+//        		j=Math.max(j, map.get(c)+1);// abba where first a again matches with last a but we can't decrease j
+//        	}
+//        	map.put(c, i);
+//        }
+//        return Math.max(len, i-j);
+//    }
 	/**
 	 * Approach 01: Sliding window
 	 * O(n)/ O(Min(M,N)) <= Important as we're going to keep the max substring in temporary variable
