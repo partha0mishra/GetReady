7f2b1c4d1eae9c7c4d25cb44d3ca2354b4d16111
Mishra <partha.mishra@gmail.com>
2021-01-07 21:09:48 -0600

    .

--- algods/src/main/java/com/leetcode/trie/WordSearch2.java
+++ algods/src/main/java/com/leetcode/trie/WordSearch2.java
@@ -32,12 +32,14 @@ All the strings of words are unique.
  */
 import java.util.*;
 public class WordSearch2 {
-	/** 
-	 * Approach 01: keeping words in a trie and then searching using backtracking
+	/**
+	 * Building on: 
+	 * Approach 01 - keeping words in a trie and then searching using backtracking [ 1558 ms -> 602 ms ] after optimizations
+	 * Approach 02 - Slimmed Trie to keep the word at the end and not a boolean 'end'
+	 * Approach 03 - at the start of backtracking, change the board[row][col] to '#' change it back to the original character at the end [< 250 ms ]
 	 */
 	class TrieNode{
-		TrieNode[] letters; boolean end;
-		public TrieNode() {letters=new TrieNode[26];end=false;}
+		TrieNode[] letters= new TrieNode[26]; String w=null;
 	}
 	class Trie{
 		TrieNode allWords;
@@ -49,23 +51,7 @@ public class WordSearch2 {
 					current.letters[c-'a']=new TrieNode();
 				current=current.letters[c-'a'];
 			}
-			current.end=true;
-		}
-		public boolean search(String word) {
-			TrieNode current=allWords;
-			for(char c: word.toCharArray()) {
-				if(current.letters[c-'a'] == null) return false;
-				current=current.letters[c-'a'];
-			}
-			return current.end;
-		}
-		public boolean startsWith(String word) {
-			TrieNode current=allWords;
-			for(char c: word.toCharArray()) {
-				if(current.letters[c-'a'] == null) return false;
-				current=current.letters[c-'a'];
-			}
-			return true;// this is just a prefix search - used for pruning
+			current.w=word;
 		}
 	}
 	public List<String> findWords(char[][] board, String[] words) {
@@ -77,56 +63,122 @@ public class WordSearch2 {
         }
         for(int row=0; row< rows; row++)
         	for(int col=0; col< cols; col++) {
-//        		System.out.println("starting with "+row+" "+col+" "+board[row][col]);
-//        		if(myTrie.startsWith(""+board[row][col])) 
-        		if(myTrie.allWords.letters[board[row][col] -'a'] != null)// following the Trie nodes - doubles performance
-        		{
-//        			System.out.println("starting with "+row+" "+col+" "+board[row][col]);
-        			HashSet<Integer> visited= new HashSet<>();
-        			visited.add(100*row+col);
-        			backtrack(row,col,rows,cols,result,""+board[row][col]
-        					, /*myTrie*/ myTrie.allWords.letters[board[row][col]-'a'] // let me carry only the relevant node
-        					, visited, board);// row no, col no, rows, cols, result, partial, trie, visited
-        		}
+        			backtrack(row,col, result, myTrie.allWords, board);
         	}
         return result;
     }
-	int[][] dirs= {{0,1},{-1,0},{0,-1},{1,0}};
-	// this time, walking through the Trie as well
-	private void backtrack(int row, int col, int rows, int cols, List<String> result, String soFar, TrieNode trieNode,
-			HashSet<Integer> visited, char[][] board) {
-		if(trieNode.end && !result.contains(soFar)) result.add(soFar);
+    int[][] dirs= {{0,1},{-1,0},{0,-1},{1,0}};
+	private void backtrack(int row, int col,List<String> result, TrieNode trieNode,
+			char[][] board) {
+        char c=board[row][col];
+        if(c== '#' || trieNode.letters[c-'a'] == null) return;
+        trieNode=trieNode.letters[c-'a'];
+		if(trieNode.w != null) {result.add(trieNode.w); trieNode.w=null;}
+        board[row][col]='#'; // used indicator instead of a hashmap
 		for(int[] dir: dirs) {
 			int newRow=row+dir[0], newCol=col+dir[1];
-			if(newRow >=0 && newCol >=0 && newRow < rows && newCol < cols 
-					&& !visited.contains(100*newRow+newCol) && trieNode.letters[board[newRow][newCol]-'a'] != null) {
-//				System.out.println(row+" "+col+" "+dir[0]+" "+dir[1]+" "+newRow+" "+newCol+" "+soFar+board[newRow][newCol]);
-				visited.add(100*newRow+newCol);
-				backtrack(newRow, newCol, rows, cols, result, soFar+board[newRow][newCol], trieNode.letters[board[newRow][newCol]-'a'], visited, board);
-				visited.remove(100*newRow+newCol);
+			if(newRow >=0 && newCol >=0 && newRow < board.length && newCol < board[0].length ) {
+				backtrack(newRow, newCol, result, trieNode, board);
 			}
 		}
-		// what if I remove the trieNode if it doesn't have a child - improves performance a lot
-		// since we don't need to use the same word anymore, also set the end to false
-		trieNode.end=false;
-		for(TrieNode tn: trieNode.letters) {
-			if(tn != null) return;
-		}
-		trieNode=null;// no child
+		board[row][col]=c; // True backtracking
 	}
-	private void backtrack(int row, int col, int rows, int cols, List<String> result, String soFar, Trie myTrie, HashSet<Integer> visited, char[][] board) {
-		if(myTrie.search(soFar) && !result.contains(soFar)) {result.add(soFar); /*System.out.println(soFar);*/}
-		for(int[] dir: dirs) {
-			int newRow=row+dir[0], newCol=col+dir[1];
-			if(newRow >=0 && newCol >=0 && newRow < rows && newCol < cols 
-					&& !visited.contains(100*newRow+newCol) && myTrie.startsWith(soFar+board[newRow][newCol])) {
-//				System.out.println(row+" "+col+" "+dir[0]+" "+dir[1]+" "+newRow+" "+newCol+" "+soFar+board[newRow][newCol]);
-				visited.add(100*newRow+newCol);
-				backtrack(newRow, newCol, rows, cols, result, soFar+board[newRow][newCol], myTrie, visited, board);
-				visited.remove(100*newRow+newCol);
-			}
-		}
-	}
+	/** 
+	 * Approach 01: keeping words in a trie and then searching using backtracking
+	 */
+//	class TrieNode{
+//		TrieNode[] letters; boolean end;
+//		public TrieNode() {letters=new TrieNode[26];end=false;}
+//	}
+//	class Trie{
+//		TrieNode allWords;
+//		public Trie() {allWords=new TrieNode();}
+//		public void add(String word) {
+//			TrieNode current=allWords;
+//			for(char c: word.toCharArray()) {
+//				if(current.letters[c-'a'] == null)
+//					current.letters[c-'a']=new TrieNode();
+//				current=current.letters[c-'a'];
+//			}
+//			current.end=true;
+//		}
+//		public boolean search(String word) {
+//			TrieNode current=allWords;
+//			for(char c: word.toCharArray()) {
+//				if(current.letters[c-'a'] == null) return false;
+//				current=current.letters[c-'a'];
+//			}
+//			return current.end;
+//		}
+//		public boolean startsWith(String word) {
+//			TrieNode current=allWords;
+//			for(char c: word.toCharArray()) {
+//				if(current.letters[c-'a'] == null) return false;
+//				current=current.letters[c-'a'];
+//			}
+//			return true;// this is just a prefix search - used for pruning
+//		}
+//	}
+//	public List<String> findWords(char[][] board, String[] words) {
+//        List<String> result=new ArrayList<>();
+//        int rows=board.length, cols=board[0].length;
+//        Trie myTrie= new Trie();
+//        for(String word: words) {
+//        	myTrie.add(word);// all words added
+//        }
+//        for(int row=0; row< rows; row++)
+//        	for(int col=0; col< cols; col++) {
+////        		System.out.println("starting with "+row+" "+col+" "+board[row][col]);
+////        		if(myTrie.startsWith(""+board[row][col])) 
+//        		if(myTrie.allWords.letters[board[row][col] -'a'] != null)// following the Trie nodes - doubles performance
+//        		{
+////        			System.out.println("starting with "+row+" "+col+" "+board[row][col]);
+//        			HashSet<Integer> visited= new HashSet<>();
+//        			visited.add(100*row+col);
+//        			backtrack(row,col,rows,cols,result,""+board[row][col]
+//        					, /*myTrie*/ myTrie.allWords.letters[board[row][col]-'a'] // let me carry only the relevant node
+//        					, visited, board);// row no, col no, rows, cols, result, partial, trie, visited
+//        		}
+//        	}
+//        return result;
+//    }
+//	int[][] dirs= {{0,1},{-1,0},{0,-1},{1,0}};
+//	// this time, walking through the Trie as well
+//	private void backtrack(int row, int col, int rows, int cols, List<String> result, String soFar, TrieNode trieNode,
+//			HashSet<Integer> visited, char[][] board) {
+//		if(trieNode.end && !result.contains(soFar)) result.add(soFar);
+//		for(int[] dir: dirs) {
+//			int newRow=row+dir[0], newCol=col+dir[1];
+//			if(newRow >=0 && newCol >=0 && newRow < rows && newCol < cols 
+//					&& !visited.contains(100*newRow+newCol) && trieNode.letters[board[newRow][newCol]-'a'] != null) {
+////				System.out.println(row+" "+col+" "+dir[0]+" "+dir[1]+" "+newRow+" "+newCol+" "+soFar+board[newRow][newCol]);
+//				visited.add(100*newRow+newCol);
+//				backtrack(newRow, newCol, rows, cols, result, soFar+board[newRow][newCol], trieNode.letters[board[newRow][newCol]-'a'], visited, board);
+//				visited.remove(100*newRow+newCol);
+//			}
+//		}
+//		// what if I remove the trieNode if it doesn't have a child - improves performance a lot
+//		// since we don't need to use the same word anymore, also set the end to false
+//		trieNode.end=false;
+//		for(TrieNode tn: trieNode.letters) {
+//			if(tn != null) return;
+//		}
+//		trieNode=null;// no child
+//	}
+	// initial try. we don't really need to check for startsWith() or search() in trie here
+//	private void backtrack(int row, int col, int rows, int cols, List<String> result, String soFar, Trie myTrie, HashSet<Integer> visited, char[][] board) {
+//		if(myTrie.search(soFar) && !result.contains(soFar)) {result.add(soFar); /*System.out.println(soFar);*/}
+//		for(int[] dir: dirs) {
+//			int newRow=row+dir[0], newCol=col+dir[1];
+//			if(newRow >=0 && newCol >=0 && newRow < rows && newCol < cols 
+//					&& !visited.contains(100*newRow+newCol) && myTrie.startsWith(soFar+board[newRow][newCol])) {
+////				System.out.println(row+" "+col+" "+dir[0]+" "+dir[1]+" "+newRow+" "+newCol+" "+soFar+board[newRow][newCol]);
+//				visited.add(100*newRow+newCol);
+//				backtrack(newRow, newCol, rows, cols, result, soFar+board[newRow][newCol], myTrie, visited, board);
+//				visited.remove(100*newRow+newCol);
+//			}
+//		}
+//	}
 	public static void main(String[] args) {
 		WordSearch2 ws2= new WordSearch2();
 		System.out.println(ws2.findWords(new char[][] {{'o','a','a','n'}
