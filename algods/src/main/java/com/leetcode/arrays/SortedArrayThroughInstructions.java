65c063fdbfd8e19545ebde6247df6e68ca4bf1d3
Mishra <partha.mishra@gmail.com>
2021-01-10 11:38:13 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/SortedArrayThroughInstructions.java
+++ algods/src/main/java/com/leetcode/arrays/SortedArrayThroughInstructions.java
@@ -57,24 +57,58 @@ Constraints:
 import static org.junit.Assert.assertEquals;
 public class SortedArrayThroughInstructions {
 	/**
-	 * Approach 01: Brute - just like Insertion Sort
+	 * Approach 02: Segment Tree
+	 */
+	 public int createSortedArray(int[] instructions) {
+	        int m = (int) 1e5 + 1;
+	        int[] tree = new int[m * 2];
+
+	        long cost = 0;
+	        long MOD = (int) 1e9 + 7;
+	        for (int x : instructions) {
+	            cost += Math.min(query(0, x, tree, m), query(x + 1, m, tree, m));
+	            update(x, 1, tree, m);
+	        }
+	        return (int) (cost % MOD);
+	    }
+
+	    // implement Segment Tree
+	    private void update(int index, int value, int[] tree, int m) {
+	        index += m;
+	        tree[index] += value;
+	        for (index >>= 1; index > 0; index >>= 1)
+	            tree[index] = tree[index << 1] + tree[(index << 1) + 1];
+	    }
+
+	    private int query(int left, int right, int[] tree, int m) {
+	        int result = 0;
+	        for (left += m, right += m; left < right; left >>= 1, right >>= 1) {
+	            if ((left & 1) == 1)
+	                result += tree[left++];
+	            if ((right & 1) == 1)
+	                result += tree[--right];
+	        }
+	        return result;
+	    }
+	/**
+	 * Approach 01: Brute - just like Insertion Sort : TLE as expected
 	 * O(N^2)/ O(1)
 	 */
-	public int createSortedArray(int[] instructions) {
-		int result=0;
-        for(int i=1; i< instructions.length; i++) {
-        	int current=instructions[i], min=0, max=0;
-//        	System.out.println(current);
-        	for(int j=0; j<i; j++) {
-//        		System.out.println(instructions[j]);
-        		if(instructions[j] < current) min+=1;
-        		if(instructions[j] > current) max+=1;
-        	}
-        	result+=Math.min(min, max)%1000000007;
-        	result%=1000000007;
-        }
-        return result%1000000007;
-    }
+//	public int createSortedArray(int[] instructions) {
+//		int result=0;
+//        for(int i=1; i< instructions.length; i++) {
+//        	int current=instructions[i], min=0, max=0;
+////        	System.out.println(current);
+//        	for(int j=0; j<i; j++) {
+////        		System.out.println(instructions[j]);
+//        		if(instructions[j] < current) min+=1;
+//        		if(instructions[j] > current) max+=1;
+//        	}
+//        	result+=Math.min(min, max)%1000000007;
+//        	result%=1000000007;
+//        }
+//        return result%1000000007;
+//    }
 	public static void main(String[] a) {
 		SortedArrayThroughInstructions sati= new SortedArrayThroughInstructions();
 		assertEquals(1,sati.createSortedArray(new int[] {1,5,6,2}));
