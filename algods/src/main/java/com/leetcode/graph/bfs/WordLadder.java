72908adf6ade32efaeb8f6cd1db08a3b17dbdcd9
Mishra <partha.mishra@gmail.com>
2021-01-09 18:48:41 -0600

    .

--- algods/src/main/java/com/leetcode/graph/bfs/WordLadder.java
+++ algods/src/main/java/com/leetcode/graph/bfs/WordLadder.java
@@ -29,8 +29,35 @@ beginWord, endWord, and wordList[i] consist of lowercase English letters.
 beginWord != endWord
 All the strings in wordList are unique.
  */
+import java.util.*;
 public class WordLadder {
-
+	/**
+	 * Approach 01: BFS as 'production sequence' paradigm
+	 */
+	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int dist=0; HashSet<String> dict= new HashSet<>(wordList);
+        Deque<String> queue= new ArrayDeque<>();
+        queue.offerLast(beginWord);
+        while(!queue.isEmpty()) {
+        	int size=queue.size();
+        	for(int s=0; s< size; s++) {
+        		String current=queue.pollFirst();
+        		if(current.equals(endWord)) return dist;
+        		int len=current.length();
+        		for(int l=0; l< len; l++) {
+        			String first= (l==0)? "": current.substring(0,l);
+        			String last=  (l== len-1)? "": current.substring(l+1);
+        			for(int i=0; i< 26; i++) {
+        				char mid=(char)('a'+i);
+        				String next=first+mid+last;
+        				if(dict.remove(next)) queue.offerLast(next);
+        			}
+        		}
+        	}
+        	dist+=1;
+        }
+        return dist;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
