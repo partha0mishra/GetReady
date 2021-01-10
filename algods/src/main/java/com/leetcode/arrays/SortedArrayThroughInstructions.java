79ab77416b2baf29f6441fd129eef53364c1ff18
Mishra <partha.mishra@gmail.com>
2021-01-10 11:44:48 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/SortedArrayThroughInstructions.java
+++ algods/src/main/java/com/leetcode/arrays/SortedArrayThroughInstructions.java
@@ -57,41 +57,155 @@ Constraints:
 import static org.junit.Assert.assertEquals;
 public class SortedArrayThroughInstructions {
 	/**
-	 * Approach 03: Binary Indexed Tree
+	 * Approach 04: Merge Sort - elements jumping from left to right during Stable sort
+	 * O(N logN)/ O(N) 
 	 */
-	 public int createSortedArray(int[] instructions) {
-	        int m = 100002;
-	        int[] bit = new int[m];
-	        long cost = 0;
-	        long MOD = 1000000007;
+    int[] smaller;
+    int[] larger;
+    int[][] temp; // record some temporal information
 
-	        for (int i = 0; i < instructions.length; i++) {
-	            int leftCost = query(instructions[i] - 1, bit);
-	            int rightCost = i - query(instructions[i], bit);
-	            cost += Math.min(leftCost, rightCost);
-	            update(instructions[i], 1, bit, m);
-	        }
-	        return (int) (cost % MOD);
-	    }
+    public int createSortedArray(int[] instructions) {
+        int n = instructions.length;
+        smaller = new int[n];
+        larger = new int[n];
+        temp = new int[n][];
+        long cost = 0;
+        long MOD = 1000000007;
 
-	    // implement Binary Index Tree
-	    private void update(int index, int value, int[] bit, int m) {
-	        index++;
-	        while (index < m) {
-	            bit[index] += value;
-	            index += index & -index;
-	        }
-	    }
+        int[][] arrSmaller = new int[n][];
+        int[][] arrLarger = new int[n][];
+        for (int i = 0; i < n; i++) {
+            arrSmaller[i] = new int[] { instructions[i], i };
+            arrLarger[i] = new int[] { instructions[i], i };
+        }
 
-	    private int query(int index, int[] bit) {
-	        index++;
-	        int result = 0;
-	        while (index >= 1) {
-	            result += bit[index];
-	            index -= index & -index;
-	        }
-	        return result;
-	    }
+        sortSmaller(arrSmaller, 0, n - 1);
+        sortLarger(arrLarger, 0, n - 1);
+
+        for (int i = 0; i < n; i++) {
+            cost += Math.min(smaller[i], larger[i]);
+        }
+        return (int) (cost % MOD);
+    }
+
+    private void sortSmaller(int[][] arr, int left, int right) {
+        if (left == right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        sortSmaller(arr, left, mid);
+        sortSmaller(arr, mid + 1, right);
+        mergeSmaller(arr, left, right, mid);
+    }
+
+    private void mergeSmaller(int[][] arr, int left, int right, int mid) {
+        // merge [left, mid] and [mid+1, right]
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        // use temp[left...right] to temporarily store sorted array
+        while (i <= mid && j <= right) {
+            if (arr[i][0] < arr[j][0]) {
+                temp[k++] = arr[i];
+                i++;
+            } else {
+                temp[k++] = arr[j];
+                smaller[arr[j][1]] += i - left;
+                j++;
+            }
+        }
+        while (i <= mid) {
+            temp[k++] = arr[i];
+            i++;
+        }
+        while (j <= right) {
+            temp[k++] = arr[j];
+            smaller[arr[j][1]] += i - left;
+            j++;
+        }
+        // restore from temp
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i];
+        }
+    }
+
+    private void sortLarger(int[][] arr, int left, int right) {
+        if (left == right) {
+            return;
+        }
+        int mid = (left + right) / 2;
+        sortLarger(arr, left, mid);
+        sortLarger(arr, mid + 1, right);
+        mergeLarger(arr, left, right, mid);
+    }
+
+    private void mergeLarger(int[][] arr, int left, int right, int mid) {
+        // merge [left, mid] and [mid+1, right]
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        // use temp[left...right] to temporarily store sorted array
+        while (i <= mid && j <= right) {
+            if (arr[i][0] <= arr[j][0]) {
+                temp[k++] = arr[i];
+                i++;
+            } else {
+                temp[k++] = arr[j];
+                larger[arr[j][1]] += mid - i + 1;
+                j++;
+            }
+        }
+        while (i <= mid) {
+            temp[k++] = arr[i];
+            i++;
+        }
+        while (j <= right) {
+            temp[k++] = arr[j];
+            larger[arr[j][1]] += mid - i + 1;
+            j++;
+        }
+        // restore from temp
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i];
+        }
+    }
+	/**
+	 * Approach 03: Binary Indexed Tree
+	 * O(N logM)/ O(M)
+	 */
+//	 public int createSortedArray(int[] instructions) {
+//	        int m = 100002;
+//	        int[] bit = new int[m];
+//	        long cost = 0;
+//	        long MOD = 1000000007;
+//
+//	        for (int i = 0; i < instructions.length; i++) {
+//	            int leftCost = query(instructions[i] - 1, bit);
+//	            int rightCost = i - query(instructions[i], bit);
+//	            cost += Math.min(leftCost, rightCost);
+//	            update(instructions[i], 1, bit, m);
+//	        }
+//	        return (int) (cost % MOD);
+//	    }
+//
+//	    // implement Binary Index Tree
+//	    private void update(int index, int value, int[] bit, int m) {
+//	        index++;
+//	        while (index < m) {
+//	            bit[index] += value;
+//	            index += index & -index;
+//	        }
+//	    }
+//
+//	    private int query(int index, int[] bit) {
+//	        index++;
+//	        int result = 0;
+//	        while (index >= 1) {
+//	            result += bit[index];
+//	            index -= index & -index;
+//	        }
+//	        return result;
+//	    }
 	/**
 	 * Approach 02: Segment Tree
 	 * O(N logM)/ O(M)
