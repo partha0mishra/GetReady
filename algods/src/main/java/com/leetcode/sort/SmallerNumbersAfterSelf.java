15604d0667c327641ef7a949654ac7d8df14a2a0
Mishra <partha.mishra@gmail.com>
2021-01-12 18:51:20 -0600

    .

---
+++ algods/src/main/java/com/leetcode/sort/SmallerNumbersAfterSelf.java
@@ -0,0 +1,28 @@
+package com.leetcode.sort;
+/*
+ * 
+ */
+import java.util.*;
+public class SmallerNumbersAfterSelf {
+	class NumIndex implements Comparable<NumIndex>{
+        int num, index;
+        public NumIndex(int n, int i){this.num=n; this.index=i;}
+        public int compareTo(NumIndex ni){
+            return Integer.compare(this.num, ni.num);
+        }
+    }
+    public List<Integer> countSmaller(int[] nums) {
+        int len=nums.length;
+        NumIndex[] nis= new NumIndex[len];
+        for(int i=0; i< len; i++) nis[i]=new NumIndex(nums[i],i);
+        Arrays.sort(nis);
+        Integer[] result=new Integer[len];
+        for(int i=0; i< len; i++) result[nis[i].index]= (i-nis[i].index <=0)? 0: (i-nis[i].index);
+        return Arrays.asList(result);
+    }
+	public static void main(String[] args) {
+		SmallerNumbersAfterSelf snas= new SmallerNumbersAfterSelf();
+		System.out.println(snas.countSmaller(new int[] {5,2,6,1}));
+	}
+
+}
