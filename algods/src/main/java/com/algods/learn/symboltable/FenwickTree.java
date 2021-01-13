6c80006337fe3aa9472ea2fd3f9218eabce43f25
Mishra <partha.mishra@gmail.com>
2021-01-12 19:13:32 -0600

    .

--- algods/src/main/java/com/algods/learn/symboltable/FenwickTree.java
+++ algods/src/main/java/com/algods/learn/symboltable/FenwickTree.java
@@ -1,10 +1,25 @@
 package com.algods.learn.symboltable;
-
+/**
+ * Binary Indexed Tree (BIT) aka Fenwick Tree
+ * Create  : O(N logN)
+ * Update  : O(  logN)
+ * SumRange: O(  logN) <= O(H)
+ * Space   : O(MAX +2) <= MAX = max number in the array
+ */
 public class FenwickTree {
 	int[] items;
 	int mod=1000000007;
 	public FenwickTree(int length) {
 		items=new int[length];
+	}
+	public FenwickTree(int[] arr) {
+		int max=0;
+		for(int a: arr) max=Math.max(max, a);
+		items=new int[max+2];// for 5 elements, I need from 0 (dummy) to 6
+		// Creating the tree is just like update
+		for(int i=0; i< arr.length; i++) {
+			update(arr[i],1);
+		}
 	}
 	public int sumRange(int start, int end) {
 		return sum(end+1)-sum(start);// 2 to 5 includes element at 2
@@ -18,7 +33,7 @@ public class FenwickTree {
 		}
 		return (int)sum;
 	}
-	public void update(int index, int val) {
+	public void update(int index, int val) {// same as create
 		index+=1;
 		while(index < items.length) {
 			items[index]+=val;
