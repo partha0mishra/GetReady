d1d01cfec400eed03e58ac720424891896721163
Mishra <partha.mishra@gmail.com>
2021-01-13 22:20:10 -0600

    .

--- algods/src/main/java/com/leetcode/design/FlattenNestedListIterator.java
+++ algods/src/main/java/com/leetcode/design/FlattenNestedListIterator.java
@@ -1,6 +1,6 @@
 package com.leetcode.design;
 /** TODO Anki
- * Flatten Nested List Iterator
+ * 341. Flatten Nested List Iterator
  * Given a nested list of integers, implement an iterator to flatten it.
 
 Each element is either an integer, or a list -- whose elements may also be integers or other lists.
