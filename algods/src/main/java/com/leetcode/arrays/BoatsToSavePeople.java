063a28490a92d2449d44f88e505284c2296eb79d
Mishra <partha.mishra@gmail.com>
2021-01-13 19:00:07 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/BoatsToSavePeople.java
+++ algods/src/main/java/com/leetcode/arrays/BoatsToSavePeople.java
@@ -35,14 +35,15 @@ public class BoatsToSavePeople {
 	 * O(N)/ O(N)
 	 */
 	public int numRescueBoats(int[] people, int limit) {
-       int[] count=new int[limit+1];
-       for(int p: people) count[p]+=1;// populate counts
-       for(int i=1; i< count.length; i++) count[i]+=count[i-1];
+       int[] count=new int[limit+1];// to keep the count
+       for(int p: people) count[p]+=1;// populate counts [1,1,2,2,2,3,3,4,5,5,3,3] becomes [0, 2, 3, 4, 1, 2]
+       for(int i=1; i< count.length; i++) count[i]+=count[i-1];// cumulative counts [0, 2, 5, 9, 10, 12] 
+       // means 1 should be at position 0 and 1; 2 should be at 2, 3, 4;
 //       for(int c: count) System.out.println(c);
        int[] sorted= new int[people.length];
-       for(int i=0; i< people.length; i++) sorted[count[people[i]-1]++]=people[i];// now it's sorted
+       for(int i=0; i< people.length; i++) sorted[count[people[i]-1]++]=people[i];// now it's sorted. notice people[i]-1
 //       for(int s: sorted) System.out.println(s);
-       int left=0, right=people.length-1, boats=0;
+       int left=0, right=people.length-1, boats=0;// same as Approach 01 from here on.
        while(left <= right) {
        	int delta=limit-sorted[right];
        	if(sorted[left]<=delta) left+=1;
@@ -58,12 +59,12 @@ public class BoatsToSavePeople {
 	 */
 //	public int numRescueBoats(int[] people, int limit) {
 //        int left=0, right=people.length-1, boats=0;
-//        Arrays.sort(people);
-//        while(left <= right) {
-//        	int delta=limit-people[right];
-//        	if(people[left]<=delta) left+=1;
-//        	boats+=1;
-//        	right-=1;
+//        Arrays.sort(people);// O(N logN)
+//        while(left <= right) {// O(N)
+//        	int delta=limit-people[right];// space left
+//        	if(people[left]<=delta) left+=1;// small one can be accomodated
+//        	boats+=1;// in any case
+//        	right-=1;// in any case
 //        }
 //        return boats;
 //    }
