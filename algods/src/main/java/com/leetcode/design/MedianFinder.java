fb9f9deffc9b6fa12295ddaddd4aa68d0402a3b5
Mishra <partha.mishra@gmail.com>
2021-01-13 21:53:29 -0600

    .

--- algods/src/main/java/com/leetcode/design/MedianFinder.java
+++ algods/src/main/java/com/leetcode/design/MedianFinder.java
@@ -1,7 +1,8 @@
 package com.leetcode.design;
 /** TODO Anki
  * 295. Find Median from Data Stream
- * Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+ * Median is the middle value in an ordered integer list. If the size of the list is even, 
+ * there is no middle value. So the median is the mean of the two middle value.
 for example,
 [2,3,4], the median is 3
 [2,3], the median is (2 + 3) / 2 = 2.5
