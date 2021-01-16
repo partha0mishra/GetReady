7db545484de9e001685ed8b9c22caf9075e2700b
Mishra <partha.mishra@gmail.com>
2021-01-15 21:46:54 -0600

    .

--- algods/src/main/java/com/leetcode/bst/BinaryTreeGoodLeafNodePairs.java
+++ algods/src/main/java/com/leetcode/bst/BinaryTreeGoodLeafNodePairs.java
@@ -52,38 +52,36 @@ public class BinaryTreeGoodLeafNodePairs {
 	 * The intuition is that, if a leaf node is at distance i from current node, it would be at distance i+1 from its parent. 
 	 * Hence, will building the res array, we are adding sum in i+1 th position and return to parent.
 	 */
-	class Solution {
-	    int result = 0;
-	    public int countPairs(TreeNode root, int distance) {
-	        dfs(root,distance);
-	        return result;
-	    }
-	    
-	    int[] dfs(TreeNode root,int distance){
-	        if(root == null)
-	            return new int[distance+1];
-	        if(root.left == null && root.right == null){
-	            int res[] = new int[distance+1];
-	            res[1]++;
-	            return res;
-	        }
-	        int[] left = dfs(root.left,distance);
-	        int[] right = dfs(root.right,distance);
-	        for(int l=1;l<left.length;l++){
-	            for(int r = distance-1;r>=0;r--){
-	                if(l+r <=distance)
-	                result += left[l]*right[r];
-	            }
-	        }
-	        int res[] = new int[distance+1];
-	        //shift by 1
-	        for(int i=res.length-2;i>=1;i--){
-	            res[i+1] = left[i]+right[i];
-	        }
-	        
-	        return res;
-	    }
-	}
+    int result = 0;
+    public int countPairs(TreeNode root, int distance) {
+        dfs(root,distance);
+        return result;
+    }
+    
+    int[] dfs(TreeNode root,int distance){
+        if(root == null)
+            return new int[distance+1];
+        if(root.left == null && root.right == null){
+            int res[] = new int[distance+1];
+            res[1]++;
+            return res;
+        }
+        int[] left = dfs(root.left,distance);
+        int[] right = dfs(root.right,distance);
+        for(int l=1;l<left.length;l++){
+            for(int r = distance-1;r>=0;r--){
+                if(l+r <=distance)
+                result += left[l]*right[r];
+            }
+        }
+        int res[] = new int[distance+1];
+        //shift by 1
+        for(int i=res.length-2;i>=1;i--){
+            res[i+1] = left[i]+right[i];
+        }
+        
+        return res;
+    }
 	/**
 	 * Approach 01 in July: copied from GFG and got TLE. Didn't understand much
 	 * Approach 02 below: 
