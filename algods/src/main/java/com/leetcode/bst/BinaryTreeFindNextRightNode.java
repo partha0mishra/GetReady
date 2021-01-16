9bd18e29c9fc875bad9e019124876da40ca3caf7
Mishra <partha.mishra@gmail.com>
2021-01-15 19:52:11 -0600

    .

--- algods/src/main/java/com/leetcode/bst/BinaryTreeFindNextRightNode.java
+++ algods/src/main/java/com/leetcode/bst/BinaryTreeFindNextRightNode.java
@@ -31,26 +31,49 @@ u is a node in the binary tree rooted at root.
  */
 import java.util.*;
 public class BinaryTreeFindNextRightNode {
-	/* Approach 01: BFS on BST
-	 * O(N) - visiting each node at worst case
-	 * O(2^h-1) - max nodes in a level */
-	TreeNode result= null;
+	/**
+	 * BFS. If node found, see if it's the last one in the level.
+	 * If so, return null. else return queue.pollFirst().
+	 * NOTE: don't insert null nodes
+	 */
 	public TreeNode findNearestRightNode(TreeNode root, TreeNode u) {
+		// if(root == null) return null;
 		Deque<TreeNode> queue=new ArrayDeque<TreeNode>();
 		queue.offerLast(root);
-		boolean found=false;
 		while(!queue.isEmpty()) {
 			int size=queue.size();
-			for(int i=0; i< size; i++) {
+			for(int s=0; s< size; s++) {
 				TreeNode tn=queue.pollFirst();
-				if(tn == null) continue;// skip
-				if(found) {result=tn; break;}
-				if(tn.val==u.val) found=true;
+				if(tn==u) {// node found
+					if(s == size-1) return null;// this is the last node of the level
+                    else return queue.pollFirst();// this is the next node of the level
+				}
 				if(tn.left != null) queue.offerLast(tn.left);
 				if(tn.right != null) queue.offerLast(tn.right);
 			}
-			if(found) break;// either next node is there or none left at this level
 		}
-        return result;
+        return null;
     }
+	/* Approach 01: BFS on BST
+	 * O(N) - visiting each node at worst case
+	 * O(2^h-1) - max nodes in a level */
+//	TreeNode result= null;
+//	public TreeNode findNearestRightNode(TreeNode root, TreeNode u) {
+//		Deque<TreeNode> queue=new ArrayDeque<TreeNode>();
+//		queue.offerLast(root);
+//		boolean found=false;
+//		while(!queue.isEmpty()) {
+//			int size=queue.size();
+//			for(int i=0; i< size; i++) {
+//				TreeNode tn=queue.pollFirst();
+//				if(tn == null) continue;// skip
+//				if(found) {result=tn; break;}
+//				if(tn.val==u.val) found=true;
+//				if(tn.left != null) queue.offerLast(tn.left);
+//				if(tn.right != null) queue.offerLast(tn.right);
+//			}
+//			if(found) break;// either next node is there or none left at this level
+//		}
+//        return result;
+//    }
 }
