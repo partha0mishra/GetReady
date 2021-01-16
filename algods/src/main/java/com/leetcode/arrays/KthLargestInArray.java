df2a146dbe94bfb35483545461ae6901875b4924
Mishra <partha.mishra@gmail.com>
2021-01-16 13:05:41 -0600

    .

---
+++ algods/src/main/java/com/leetcode/arrays/KthLargestInArray.java
@@ -0,0 +1,39 @@
+package com.leetcode.arrays;
+/**
+ * Kth Largetst Element in an Array
+ * Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+ */
+import java.util.*;
+import static org.junit.Assert.assertEquals;
+public class KthLargestInArray {
+	/**
+	 * TreeSet as PQ, Sort Descending and allow duplicates
+	 * insert O(N logN), delete O(1) for K elements
+	 */
+	public int findKthLargest(int[] nums, int k) {
+		TreeSet<Integer> pq= new TreeSet<>((i1,i2) -> {
+			int diff=Integer.compare(i2,i1);
+			return (diff == 0)? 1: diff;
+		});
+		for(int n: nums) pq.add(n);
+		for(int i=1; i< k; i++) System.out.println(pq.pollFirst());
+		return pq.pollFirst();
+    }
+	public static void main(String[] args) {
+		KthLargestInArray klia=new KthLargestInArray();
+		assertEquals(5,klia.findKthLargest(new int[] {3,2,1,5,6,4}, 2));
+		assertEquals(4,klia.findKthLargest(new int[] {3,2,3,1,2,4,5,5,6}, 4));
+	}
+
+}
