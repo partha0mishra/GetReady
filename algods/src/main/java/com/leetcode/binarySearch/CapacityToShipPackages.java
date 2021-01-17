7197b44923e614ffba5070e9320a97dffc34fed2
Mishra <partha.mishra@gmail.com>
2021-01-16 22:17:16 -0600

    .

--- algods/src/main/java/com/leetcode/binarySearch/CapacityToShipPackages.java
+++ algods/src/main/java/com/leetcode/binarySearch/CapacityToShipPackages.java
@@ -6,7 +6,8 @@ package com.leetcode.binarySearch;
  * The i-th package on the conveyor belt has a weight of weights[i].  
  * Each day, we load the ship with packages on the conveyor belt (in the order given by weights). 
  * We may not load more weight than the maximum weight capacity of the ship.
- * Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within D days.
+ * Return the least weight capacity of the ship that will result in all the packages 
+ * on the conveyor belt being shipped within D days.
  * 
  * Example 1:
  * Input: weights = [1,2,3,4,5,6,7,8,9,10], D = 5
@@ -81,7 +82,8 @@ public class CapacityToShipPackages {
 				count++;
 				if(count == days) return false;
 			}
-		}
+		}// count < days and total < capacity. 
+		// So, in worst case, the last day will take care of the residual in 'total'
 		return true;
 	}
 	public static void main(String[] args) {
