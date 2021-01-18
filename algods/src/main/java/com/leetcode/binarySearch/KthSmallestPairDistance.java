d1b082c6a4a8253d70b3939e837ed2c9e03c04bf
Mishra <partha.mishra@gmail.com>
2021-01-17 22:55:06 -0600

    .

--- algods/src/main/java/com/leetcode/binarySearch/KthSmallestPairDistance.java
+++ algods/src/main/java/com/leetcode/binarySearch/KthSmallestPairDistance.java
@@ -27,7 +27,10 @@ public class KthSmallestPairDistance {
 	/**
 	 * Very similar to LC 668 above, both are about finding Kth-Smallest. 
 	 * Just like LC 668, We can design an enough function, given an input distance, determine whether there're at least k pairs 
-	 * whose distances are less than or equal to distance. We can sort the input array and use two pointers 
+	 * whose distances are less than or equal to distance. 
+	 * 
+	 * How to find distances betwen pairs? Sort the Array, take 2 pointers
+	 * We can sort the input array and use two pointers 
 	 * (fast pointer and slow pointer, pointed at a pair) to scan it. Both pointers go from leftmost end. 
 	 * If the current pair pointed at has a distance less than or equal to distance, all pairs between these pointers are valid 
 	 * (since the array is already sorted), we move forward the fast pointer. Otherwise, we move forward the slow pointer. 
