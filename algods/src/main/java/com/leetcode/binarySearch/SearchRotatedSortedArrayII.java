1b0ae553718a2f3d546f5a157da09dcdf8adeb7a
Mishra <partha.mishra@gmail.com>
2021-01-18 21:44:02 -0600

    .

--- algods/src/main/java/com/leetcode/binarySearch/SearchRotatedSortedArrayII.java
+++ algods/src/main/java/com/leetcode/binarySearch/SearchRotatedSortedArrayII.java
@@ -1,5 +1,35 @@
 package com.leetcode.binarySearch;
+/**
+ * 81. Search in Rotated Sorted Array II
+ * 
+ * You are given an integer array nums sorted in ascending order (not necessarily distinct values), and an integer target.
 
+Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [0,1,2,4,4,4,5,6,6,7] might become [4,5,6,6,7,0,1,2,4,4]).
+
+If target is found in the array return its index, otherwise, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
+ 
+
+Follow up: This problem is the same as Search in Rotated Sorted Array, where nums may contain duplicates. Would this affect the run-time complexity? How and why?
+ */
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 public class SearchRotatedSortedArrayII {
