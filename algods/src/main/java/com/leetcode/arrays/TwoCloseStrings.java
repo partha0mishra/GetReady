221f576c41e2d22a86f033616f0fc7bf3fdaa36c
partha0mishra <partha.mishra@gmail.com>
2021-01-22 21:17:32 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/TwoCloseStrings.java
+++ algods/src/main/java/com/leetcode/arrays/TwoCloseStrings.java
@@ -53,32 +53,6 @@ import static org.junit.Assert.assertFalse;
 import java.util.*;
 public class TwoCloseStrings {
 	/**
-	 * Keeping counts in a map <char, count>
-	 * O(N)/ O(1) but obviously better than the rough implementation
-	 */
-	public boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length()) {
-            return false;
-        }
-        int word1Map[] = new int[26];
-        int word2Map[] = new int[26];
-        for (char c : word1.toCharArray()) {
-            word1Map[c - 'a']++;
-        }
-        for (char c : word2.toCharArray()) {
-            word2Map[c - 'a']++;
-        }
-        for (int i = 0; i < 26; i++) {
-            if ((word1Map[i] == 0 && word2Map[i] > 0) ||
-                (word2Map[i] == 0 && word1Map[i] > 0)) {
-                return false;
-            }
-        }
-        Arrays.sort(word1Map);
-        Arrays.sort(word2Map);
-        return Arrays.equals(word1Map, word2Map);
-    }
-	/**
 	 * Rough implementation
 	 * O(N)/ O(1)
 	 */
