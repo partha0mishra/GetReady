0a9f42c46bbf1116b7fb86413fcb1d024ec11353
partha0mishra <partha.mishra@gmail.com>
2021-01-22 21:17:17 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/TwoCloseStrings.java
+++ algods/src/main/java/com/leetcode/arrays/TwoCloseStrings.java
@@ -52,6 +52,36 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import java.util.*;
 public class TwoCloseStrings {
+	/**
+	 * Keeping counts in a map <char, count>
+	 * O(N)/ O(1) but obviously better than the rough implementation
+	 */
+	public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+        int word1Map[] = new int[26];
+        int word2Map[] = new int[26];
+        for (char c : word1.toCharArray()) {
+            word1Map[c - 'a']++;
+        }
+        for (char c : word2.toCharArray()) {
+            word2Map[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if ((word1Map[i] == 0 && word2Map[i] > 0) ||
+                (word2Map[i] == 0 && word1Map[i] > 0)) {
+                return false;
+            }
+        }
+        Arrays.sort(word1Map);
+        Arrays.sort(word2Map);
+        return Arrays.equals(word1Map, word2Map);
+    }
+	/**
+	 * Rough implementation
+	 * O(N)/ O(1)
+	 */
 	public boolean closeStrings(String word1, String word2) {
         if(word1.length() != word2.length()) return false;
         int[] count1= new int[26], count2=new int[26];
@@ -68,10 +98,10 @@ public class TwoCloseStrings {
         if(letters1.size() != letters2.size()) return false;
         Arrays.sort(count1);
         Arrays.sort(count2);
-        for(int i=0; i<26; i++) {
-        	if(count1[i] != count2[i]) return false;
-        }
-        return true;
+//        for(int i=0; i<26; i++) {
+//        	if(count1[i] != count2[i]) return false;
+//        }
+        return Arrays.equals(count1, count2);
     }
 	public static void main(String[] args) {
 		assertTrue(new TwoCloseStrings().closeStrings("abc", "bca"));
