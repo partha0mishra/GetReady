e538eb9360d282a420c46fb5f49595967ba8be0c
partha0mishra <partha.mishra@gmail.com>
2021-01-24 20:30:50 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/CompareVersionNumbers.java
+++ algods/src/main/java/com/leetcode/_explore/CompareVersionNumbers.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /**
  * Compare two version numbers version1 and version2.
  * If version1 > version2 return 1; if version1 < version2 return -1;otherwise return 0.
