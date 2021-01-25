09f98aa321a68c59f28579324afaca8c42f19274
partha0mishra <partha.mishra@gmail.com>
2021-01-24 20:32:44 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/FindAllDuplicates.java
+++ algods/src/main/java/com/leetcode/_explore/FindAllDuplicates.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /*
  * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
  * Find all the elements that appear twice in this array.
