0b99f5243445c99152fee7802abf711359c26f60
partha0mishra <partha.mishra@gmail.com>
2021-01-24 20:29:22 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/ArrangingCoins.java
+++ algods/src/main/java/com/leetcode/_explore/ArrangingCoins.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /**
  * 441. Arranging Coins
  * You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
