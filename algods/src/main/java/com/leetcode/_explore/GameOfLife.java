aa4e9be6d31751ae8515f3d4a6d616da87426221
partha0mishra <partha.mishra@gmail.com>
2021-01-24 20:33:30 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/GameOfLife.java
+++ algods/src/main/java/com/leetcode/_explore/GameOfLife.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /**
  * Game of Life
  * According to Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
