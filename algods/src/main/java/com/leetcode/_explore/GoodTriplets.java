613c2334b7b05e3ac373c22474e61cc2ea5ab772
partha0mishra <partha.mishra@gmail.com>
2021-01-24 20:34:33 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/GoodTriplets.java
+++ algods/src/main/java/com/leetcode/_explore/GoodTriplets.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /*
  * WEEKLY Contest 200
  * Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
