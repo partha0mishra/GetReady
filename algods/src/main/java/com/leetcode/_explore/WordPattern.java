c2be784dde48a32d94ab56eada2d55f8aa5c7c1a
partha0mishra <partha.mishra@gmail.com>
2021-01-26 21:38:05 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/WordPattern.java
+++ algods/src/main/java/com/leetcode/_explore/WordPattern.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /**
  * Given a pattern and a string str, find if str follows the same pattern.
  * Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
