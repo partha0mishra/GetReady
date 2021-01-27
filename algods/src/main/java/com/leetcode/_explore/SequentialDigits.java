b681bd4d7ccd34d36e466f5fc983da86dbd1aeac
partha0mishra <partha.mishra@gmail.com>
2021-01-26 21:36:08 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/SequentialDigits.java
+++ algods/src/main/java/com/leetcode/_explore/SequentialDigits.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /*
  * An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
  * Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
