99dfa828bc7ec2b36d3299bd2e3f61ca550e48f3
partha0mishra <partha.mishra@gmail.com>
2021-01-26 21:34:42 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/RearrangeSpaces.java
+++ algods/src/main/java/com/leetcode/_explore/RearrangeSpaces.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /*
  * Rearrange Spaces Between Words
  * You are given a string text of words that are placed among some number of spaces. Each word consists of one or more lowercase English letters and are separated by at least one space. It's guaranteed that text contains at least one word.
