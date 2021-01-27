ec80ac8657ea4a0bad89b58c79c9f61f2c5ec8e4
partha0mishra <partha.mishra@gmail.com>
2021-01-26 21:36:14 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/ShortestWordDistance.java
+++ algods/src/main/java/com/leetcode/_explore/ShortestWordDistance.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /* 243. Shortest Word Distance
  * 
  * Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
