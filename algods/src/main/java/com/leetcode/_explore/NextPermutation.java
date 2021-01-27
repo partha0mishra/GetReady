26020f7ed39a22327ad51d3357b47d07857a794c
partha0mishra <partha.mishra@gmail.com>
2021-01-26 21:32:08 -0600

    .

--- algods/src/main/java/com/leetcode/_explore/NextPermutation.java
+++ algods/src/main/java/com/leetcode/_explore/NextPermutation.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.leetcode._explore;
 /* 31. Next Permutation
  * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
 
