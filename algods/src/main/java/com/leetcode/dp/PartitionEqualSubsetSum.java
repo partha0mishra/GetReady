ccd2d729db4ae0b760672be3c9030722e1cabf57
partha0mishra <partha.mishra@gmail.com>
2021-01-29 20:32:42 -0600

    .

--- algods/src/main/java/com/leetcode/dp/PartitionEqualSubsetSum.java
+++ algods/src/main/java/com/leetcode/dp/PartitionEqualSubsetSum.java
@@ -26,6 +26,25 @@ Constraints:
  */
 import java.util.*;
 public class PartitionEqualSubsetSum {
+	/**
+	 * Approach 03: DP
+	 * this is anyway 0/1 knapsack
+	 * we need only previous row, 
+	 * might as well calculate from right to preserve previous results in the same row
+	 */
+	public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int num: nums) sum+=num;
+        if((sum & 1) ==1) return false;// sum needs to be even
+        sum/=2;
+        boolean[] dp=new boolean[sum+1];
+        dp[0]=true;
+        for(int num: nums)
+        	for(int i=sum; i>0; i--) {
+//        		if(i >= num) dp[i]=dp[i-num] || dp[i];// dp[i] could be already true from last iteration
+        	}
+        return dp[sum];
+    }
 	/* Approach 02: Adding memoization 
 	 * Approach 01: 
 	 * sum has to be EVEN, of course
@@ -34,25 +53,25 @@ public class PartitionEqualSubsetSum {
 	 * 
 	 * Base case: target ==0 : true, target < 0: false;
 	 */
-	HashMap<Integer,Boolean> memo;
-	public boolean canPartition(int[] nums) {
-        int sum=0;
-        for(int n: nums) sum+=n;
-        if(sum %2 !=0 || sum==0 ) return false;
-        sum/=2;
-        memo=new HashMap<>();
-        return find(sum,nums,nums.length-1);
-    }
-	
-	private boolean find(int target, int[] nums, int n) {
-		if(target ==0 ) return true;// found
-		if(target < 0 || n< 0) return false;// can't be found
-		if(!memo.containsKey(target)) {
-			memo.put(target,
-					find(target-nums[n],nums, n-1) || find(target,nums,n-1));
-		}
-		return memo.get(target);
-	}
+//	HashMap<Integer,Boolean> memo;
+//	public boolean canPartition(int[] nums) {
+//        int sum=0;
+//        for(int n: nums) sum+=n;
+//        if(sum %2 !=0 || sum==0 ) return false;
+//        sum/=2;
+//        memo=new HashMap<>();
+//        return find(sum,nums,nums.length-1);
+//    }
+//	
+//	private boolean find(int target, int[] nums, int n) {
+//		if(target ==0 ) return true;// found
+//		if(target < 0 || n< 0) return false;// can't be found
+//		if(!memo.containsKey(target)) {
+//			memo.put(target,
+//					find(target-nums[n],nums, n-1) || find(target,nums,n-1));
+//		}
+//		return memo.get(target);
+//	}
 
 	public static void main(String[] args) {
 		PartitionEqualSubsetSum instance= new PartitionEqualSubsetSum();
