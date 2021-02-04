92bed079ddafd6a22c81e1261dec64ba94dd1c92
partha0mishra <partha.mishra@gmail.com>
2021-02-03 20:27:55 -0600

    .

--- algods/src/main/java/com/leetcode/linkedList/AddTwoNumbers2.java
+++ algods/src/main/java/com/leetcode/linkedList/AddTwoNumbers2.java
@@ -1,5 +1,6 @@
 package com.leetcode.linkedList;
-/* 445: Add Two Numbers II
+/** TODO Anki 
+ * 445: Add Two Numbers II
  * You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
@@ -14,30 +15,21 @@ Output: 7 -> 8 -> 0 -> 7
  */
 import java.util.*;
 public class AddTwoNumbers2 {
-	/* Approach 02: Using Stack
+	/** 
+	 * Approach 02: Using Stack since the operations need to be from Right
 	 * */
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1 = new Stack<Integer>(),s2 = new Stack<Integer>();
-        
-        while(l1 != null) {s1.push(l1.val);l1 = l1.next;}
-        while(l2 != null) {s2.push(l2.val);l2 = l2.next;}
-        
-        int n = 0;
-        ListNode result = null;
-        while (!s1.empty() || !s2.empty()) {
-            if (!s1.empty()) n += s1.pop();
-            if (!s2.empty()) n += s2.pop();
-            ListNode temp=new ListNode(n%10);
-            temp.next=result;
-            result=temp;
-            n /= 10;
+        Stack<Integer> sa= new Stack<>(), sb=new Stack<>();
+        while(l1 != null){sa.push(l1.val); l1=l1.next;}
+        while(l2 != null){sb.push(l2.val); l2=l2.next;}
+        int sum=0;
+        ListNode result=null, temp;
+        while(!sa.isEmpty() || !sb.isEmpty()|| sum!=0){ // NOTE: sum!=0 so that the carry is also put into a new head node
+            if(!sa.isEmpty()) sum+=sa.pop();
+            if(!sb.isEmpty()) sum+=sb.pop();
+            temp=new ListNode(sum%10); sum/=10;
+            temp.next=result; result=temp;
         }
-        if(n > 0) {
-        	ListNode temp=new ListNode(n);
-            temp.next=result;
-            result=temp;
-        }
-        
         return result;
     }
 	/* Approach 01a: using int1 to keep l1 and int2 to keep l2 and int3=int1+int2 and convert back to LL - too small
