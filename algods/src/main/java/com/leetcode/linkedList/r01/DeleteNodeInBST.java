807a69ae4581efe3921e21289c50d47e6ac7218a
partha0mishra <partha.mishra@gmail.com>
2021-02-03 20:48:29 -0600

    .

--- algods/src/main/java/com/leetcode/linkedList/r01/DeleteNodeInBST.java
+++ algods/src/main/java/com/leetcode/linkedList/r01/DeleteNodeInBST.java
@@ -1,5 +1,9 @@
-package com.leetcode.linkedList;
-/* 237 Delete Node in a Linked List
+package com.leetcode.linkedList.r01;
+
+import com.leetcode.linkedList.ListNode;
+
+/** TODO Anki
+ * 237. Delete Node in a Linked List
  * Write a function to delete a node in a singly-linked list. 
  * You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
 
