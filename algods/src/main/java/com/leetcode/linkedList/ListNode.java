4b0cdb92fe818493d1fbb18552cb8a65c1f20cfc
partha0mishra <partha.mishra@gmail.com>
2021-02-03 20:03:34 -0600

    .

--- algods/src/main/java/com/leetcode/linkedList/ListNode.java
+++ algods/src/main/java/com/leetcode/linkedList/ListNode.java
@@ -4,9 +4,9 @@ package com.leetcode.linkedList;
  * Definition for singly-linked list.
  */
 public class ListNode {
-     int val;
+     public int val;
      public ListNode next;
-     ListNode() {}
+     public ListNode() {}
      public ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next;}
+     public ListNode(int val, ListNode next) { this.val = val; this.next = next;}
 }
