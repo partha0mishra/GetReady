17810de54d41ff3d0b1bf43835ed927e1870e314
partha0mishra <partha.mishra@gmail.com>
2021-02-04 10:43:48 -0600

    .

--- algods/src/main/java/com/leetcode/hashTable/WordPattern.java
+++ algods/src/main/java/com/leetcode/hashTable/WordPattern.java
@@ -1,4 +1,4 @@
-package com.leetcode._explore;
+package com.leetcode.hashTable;
 /**
  * Given a pattern and a string str, find if str follows the same pattern.
  * Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
