7e608faa82a899a4ee12962c287f9a07602039da
partha0mishra <partha.mishra@gmail.com>
2021-02-05 21:55:48 -0600

    .

--- algods/src/main/java/com/leetcode/stack/DecodeString.java
+++ algods/src/main/java/com/leetcode/stack/DecodeString.java
@@ -1,5 +1,6 @@
 package com.leetcode.stack;
-/* 
+/** 
+ * 394. Decode String
  * Given an encoded string, return its decoded string.
 
 The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
@@ -37,31 +38,104 @@ All the integers in s are in the range [1, 300].
  */
 import java.util.*;
 public class DecodeString {
+	/**
+	 * Stack implementation
+	 * 
+	 * Approach 1: Using Stack
+Intuition
+
+We have to decode the result in a particular pattern. We know that the input is always valid. The pattern begins with a number k, followed by opening braces [, followed by string. Post that, there could be one of the following cases :
+
+There is another nested pattern k[string k[string]]
+There is a closing bracket k[string]
+Since we have to start decoding the innermost pattern first, continue iterating over the string s, pushing each character to the stack until it is not a closing bracket ]. Once we encounter the closing bracket ], we must start decoding the pattern.
+
+As we know that stack follows the Last In First Out (LIFO) Principle, the top of the stack would have the data we must decode.
+
+Algorithm
+
+The input can contain an alphabet (a-z), digit (0-9), opening braces [ or closing braces ]. Start traversing string s and process each character based on the following rules:
+
+Case 1) Current character is not a closing bracket ].
+
+Push the current character to stack.
+
+Case 2) Current character is a closing bracket ].
+
+Start decoding the last traversed string by popping the string decodedString and number k from the top of the stack.
+
+Pop from the stack while the next character is not an opening bracket [ and append each character (a-z) to the decodedString.
+Pop opening bracket [ from the stack.
+Pop from the stack while the next character is a digit (0-9) and build the number k.
+Now that we have k and decodedString , decode the pattern k[decodedString] by pushing the decodedString to stack k times.
+	 */
 	public String decodeString(String s) {
-        Deque<Character> queue = new ArrayDeque<>();
-        for (char c : s.toCharArray()) queue.offer(c);
-        return helper(queue);
-    }
-    
-    public String helper(Deque<Character> queue) {
-        StringBuilder sb = new StringBuilder();
-        int num = 0;
-        while (!queue.isEmpty()) {
-            char c= queue.poll();
-            if (Character.isDigit(c)) {
-                num = num * 10 + c - '0';
-            } else if (c == '[') {
-                String sub = helper(queue);
-                for (int i = 0; i < num; i++) sb.append(sub);   
-                num = 0;
-            } else if (c == ']') {
-                break;
-            } else {
-                sb.append(c);
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ']') {
+                List<Character> decodedString = new ArrayList<>();
+                // get the encoded string
+                while (stack.peek() != '[') {
+                    decodedString.add(stack.pop());
+                }
+                // pop [ from the stack
+                stack.pop();
+                int base = 1;
+                int k = 0;
+                // get the number k
+                while (!stack.isEmpty() && Character.isDigit(stack.peek())) {
+                    k = k + (stack.pop() - '0') * base;
+                    base *= 10;
+                }
+                // decode k[decodedString], by pushing decodedString k times into stack
+                while (k != 0) {
+                    for (int j = decodedString.size() - 1; j >= 0; j--) {
+                        stack.push(decodedString.get(j));
+                    }
+                    k--;
+                }
             }
+            // push the current character to stack
+            else {
+                stack.push(s.charAt(i));
+            }
+        }      
+        // get the result from stack
+        char[] result = new char[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
         }
-        return sb.toString();
+        return new String(result);
     }
+	
+	/**
+	 * Approach 01: recursion - copied
+	 */
+//	public String decodeString(String s) {
+//        Deque<Character> queue = new ArrayDeque<>();
+//        for (char c : s.toCharArray()) queue.offer(c);
+//        return helper(queue);
+//    }
+//    
+//    public String helper(Deque<Character> queue) {
+//        StringBuilder sb = new StringBuilder();
+//        int num = 0;
+//        while (!queue.isEmpty()) {
+//            char c= queue.poll();
+//            if (Character.isDigit(c)) {
+//                num = num * 10 + c - '0';
+//            } else if (c == '[') {
+//                String sub = helper(queue);
+//                for (int i = 0; i < num; i++) sb.append(sub);   
+//                num = 0;
+//            } else if (c == ']') {
+//                break;
+//            } else {
+//                sb.append(c);
+//            }
+//        }
+//        return sb.toString();
+//    }
     /* Approach 01: using stack - simulating recursion */
 //	public String decodeString(String s) {
 //        String res = "";
