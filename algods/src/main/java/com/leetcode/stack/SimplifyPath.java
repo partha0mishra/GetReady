4fc60d1412ac47cdc8af772f1595909e5d8ca93b
partha0mishra <partha.mishra@gmail.com>
2021-02-05 20:40:17 -0600

    .

--- algods/src/main/java/com/leetcode/stack/SimplifyPath.java
+++ algods/src/main/java/com/leetcode/stack/SimplifyPath.java
@@ -1,5 +1,5 @@
 package com.leetcode.stack;
-/**
+/** TODO Anki
  * 71. Simplify Path
  * Given a string path, which is an absolute path (starting with a slash '/') to a file or directory in a Unix-style file system, convert it to the simplified canonical path.
 
@@ -12,8 +12,6 @@ Any two directories are separated by a single slash '/'.
 The path does not end with a trailing '/'.
 The path only contains the directories on the path from the root directory to the target file or directory (i.e., no period '.' or double period '..')
 Return the simplified canonical path.
-
- 
 
 Example 1:
 
