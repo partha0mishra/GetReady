d38fbba781c88880b17e8660ddf2c6208d70d9d0
partha0mishra <partha.mishra@gmail.com>
2021-02-07 21:25:28 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/RunningSum1dArray.java
+++ algods/src/main/java/com/leetcode/arrays/RunningSum1dArray.java
@@ -3,10 +3,7 @@ package com.leetcode.arrays;
  * 1480. Running Sum of 1d Array
  * 
  * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
-
 Return the running sum of nums.
-
- 
 
 Example 1:
 
