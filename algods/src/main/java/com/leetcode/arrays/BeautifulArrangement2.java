487aa29ae9fc20ed76b55cb078e3006f7c4877e9
partha0mishra <partha.mishra@gmail.com>
2021-02-07 22:57:00 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/BeautifulArrangement2.java
+++ algods/src/main/java/com/leetcode/arrays/BeautifulArrangement2.java
@@ -1,8 +1,10 @@
 package com.leetcode.arrays;
 /**
  * 667. Beautiful Arrangement 2
- * Given two integers n and k, you need to construct a list which contains n different positive integers ranging from 1 to n and obeys the following requirement:
-Suppose this list is [a1, a2, a3, ... , an], then the list [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] has exactly k distinct integers.
+ * Given two integers n and k, you need to construct a list which contains n different positive integers ranging from 1 to n 
+ * and obeys the following requirement:
+Suppose this list is [a1, a2, a3, ... , an], 
+then the list [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] has exactly k distinct integers.
 
 If there are multiple answers, print any of them.
 
