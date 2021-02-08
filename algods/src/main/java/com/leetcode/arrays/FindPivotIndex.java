cf2f6a4897f684319d498ef6a72716ef5781a24c
partha0mishra <partha.mishra@gmail.com>
2021-02-07 21:35:33 -0600

    .

--- algods/src/main/java/com/leetcode/arrays/FindPivotIndex.java
+++ algods/src/main/java/com/leetcode/arrays/FindPivotIndex.java
@@ -1,13 +1,12 @@
 package com.leetcode.arrays;
-/* 724. Find Pivot Index
+/**
+ * 724. Find Pivot Index
  * Given an array of integers nums, write a method that returns the "pivot" index of this array.
+We define the pivot index as the index where the sum of all the numbers to the left of the index 
+is equal to the sum of all the numbers to the right of the index.
 
-We define the pivot index as the index where the sum of all the numbers to the left of the index is equal to the sum of all the numbers to the right of the index.
-
-If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
-
- 
-
+If no such index exists, we should return -1. If there are multiple pivot indexes, 
+you should return the left-most pivot index.
 Example 1:
 
 Input: nums = [1,7,3,6,5,6]
