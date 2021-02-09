8f376c8f2d6078f57e02156fef6544451f1f2b56
partha0mishra <partha.mishra@gmail.com>
2021-02-09 12:20:02 -0600

    .

--- algods/src/main/java/com/leetcode/bst/BinaryTreeToGreaterTree.java
+++ algods/src/main/java/com/leetcode/bst/BinaryTreeToGreaterTree.java
@@ -1,6 +1,7 @@
 package com.leetcode.bst;
 /**
  * 538. Convert BST to Greater Tree
+ * 1038. Binary Search Tree to Greater Sum Tree [ Identical ]
  * Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
 
 As a reminder, a binary search tree is a tree that satisfies these constraints:
