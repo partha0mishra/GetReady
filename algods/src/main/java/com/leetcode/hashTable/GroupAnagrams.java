dfae132547baa091b4b25c482e63ccbdd20f4a7b
partha0mishra <partha.mishra@gmail.com>
2021-02-11 15:34:55 -0600

    .

--- algods/src/main/java/com/leetcode/hashTable/GroupAnagrams.java
+++ algods/src/main/java/com/leetcode/hashTable/GroupAnagrams.java
@@ -3,12 +3,10 @@ package com.leetcode.hashTable;
  * 49. Group Anagrams
  * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
- 
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
 
 Example 1:
-
 Input: strs = ["eat","tea","tan","ate","nat","bat"]
 Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 Example 2:
@@ -19,8 +17,6 @@ Example 3:
 
 Input: strs = ["a"]
 Output: [["a"]]
- 
-
 Constraints:
 
 1 <= strs.length <= 104
@@ -29,6 +25,10 @@ strs[i] consists of lower-case English letters.
  */
 import java.util.*;
 public class GroupAnagrams {
+	/**
+	 * O(N)/ O(N)
+	 * actually the sorting would take mlogm where m= length of the biggest string
+	 */
 	public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map= new HashMap<>();
         for(String str: strs) {
