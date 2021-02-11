7389e170e345d4f73d5caccab98366c7c2bfc206
partha0mishra <partha.mishra@gmail.com>
2021-02-10 23:00:44 -0600

    .

--- algods/src/main/java/com/leetcode/linkedList/RotateListRight.java
+++ algods/src/main/java/com/leetcode/linkedList/RotateListRight.java
@@ -1,5 +1,6 @@
 package com.leetcode.linkedList;
-/* 61 Rotate List
+/**
+ * 61 Rotate List
  * 
  * Given a linked list, rotate the list to the right by k places, where k is non-negative.
 
@@ -21,6 +22,34 @@ rotate 3 steps to the right: 0->1->2->NULL
 rotate 4 steps to the right: 2->0->1->NULL
  * */
 public class RotateListRight {
+	/**
+	 * Approach 02:
+	 */
+	public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next==null) return head;
+        int length=1;// let's find the length first
+        ListNode current=head;
+        while(current.next !=null){
+            current=current.next;
+            length++;
+        }
+        k=k%length;// the is the 'real' rotation we need to do
+        // System.out.println(k+" "+length);
+        for(int i=1; i<= k; i++){
+            current=head;
+            while(current.next.next != null){// at the end, current.next will be the last node
+                current=current.next;
+            }
+            current.next.next=head;// connecting last node to the Head
+            head=current.next;// changing 'head' pointer
+            current.next=null;// ending the list at the previous node
+        }
+        return head;
+    }
+	/**
+	 * Approach 01:
+	 * 
+	 */
 	public ListNode rotateRight(ListNode head, int k) {
         if(head == null || head.next==null) return head;
         int length=1;// let's find the length first
