23736c76b08d1fb215201fab9b068f2753e181ad
partha0mishra <partha.mishra@gmail.com>
2021-02-11 21:19:16 -0600

    .

--- algods/src/main/java/com/leetcode/hashTable/_easy/ContainsDuplicate2.java
+++ algods/src/main/java/com/leetcode/hashTable/_easy/ContainsDuplicate2.java
@@ -1,4 +1,4 @@
-package com.leetcode.hashTable;
+package com.leetcode.hashTable._easy;
 /**
  *  219. Contains Duplicate II
  * Given an array of integers and an integer k, find out whether there are two distinct indices i and j 
